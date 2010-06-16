4a12aff1bba996aa053675bf5022e3787ab3bd18
matsek <matsek@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2010-06-16 13:08:11 +0000

    Newly created certificates with old CA

--- ServiceInteractions/riv/insuranceprocess/healthreporting/trunk/certs/gencerts.sh
+++ ServiceInteractions/riv/insuranceprocess/healthreporting/trunk/certs/gencerts.sh
@@ -26,64 +26,81 @@ echo "1346" > demoCA/serial
 # internal CXF reasons.
 
 # ### Consumers (Calling clients to TP) ###
-#   Riv13606RequestEHRExtractConsumer
+#   Consumer
     keytool -genkey \
-    -alias Riv13606RequestEHRExtractConsumer \
-    -dname "CN= Riv13606RequestEHRExtractConsumer, OU=VardgivareA-IVK, O=SKL, ST=GBG, C=SE" \
+    -alias Consumer \
+    -dname "CN=Consumer, OU=VardgivareA-IVK, O=SKL, ST=GBG, C=SE" \
     -keystore consumer.jks -storetype jks -storepass password -keypass password -keyalg RSA
 
     keytool -certreq -keystore consumer.jks -storetype jks -storepass password \
-    -alias Riv13606RequestEHRExtractConsumer \
-    -keypass password -file csrRiv13606RequestEHRExtractConsumer.pem
+    -alias Consumer \
+    -keypass password -file csrConsumer.pem
 
 # ### Producers (Backend national service) ###
-#   Riv13606RequestEHRExtractProducer
+#   Producer
     keytool -genkey \
-    -alias Riv13606RequestEHRExtractProducer \
-    -dname "CN=localhost, OU=VardgivareC-IVK, O=SKL, ST=GBG, C=SE" \
+    -alias Producer \
+    -dname "CN=producer.riv.se, OU=VardgivareB-IVK, O=SKL, ST=GBG, C=SE" \
     -keystore producer.jks -storetype jks -storepass password -keypass password -keyalg RSA
 
     keytool -certreq -keystore producer.jks -storetype jks -storepass password \
-    -alias Riv13606RequestEHRExtractProducer \
-    -keypass password -file csrRiv13606RequestEHRExtractProducer.pem
+    -alias Producer \
+    -keypass password -file csrProducer.pem
 
+#   ProducerLocalhost
+    keytool -genkey \
+    -alias ProducerLocalhost \
+    -dname "CN=localhost, OU=VardgivareC-IVK, O=SKL, ST=GBG, C=SE" \
+    -keystore producer-localhost.jks -storetype jks -storepass password -keypass password -keyalg RSA
+
+    keytool -certreq -keystore producer-localhost.jks -storetype jks -storepass password \
+    -alias ProducerLocalhost \
+    -keypass password -file csrProducerLocalhost.pem
 
 # Have the CN=TheCA issue a certificate for Consumers and Producers via
 # their Certificate Requests.
 #  Consumers 
-   openssl ca -batch -days 364 -cert cacert.pem -keyfile caprivkey.pem \
-   -in csrRiv13606RequestEHRExtractConsumer.pem -out Riv13606RequestEHRExtractConsumer-ca-cert.pem
+   openssl ca -batch -days 7200 -cert cacert.pem -keyfile caprivkey.pem \
+   -in csrConsumer.pem -out Consumer-ca-cert.pem
 #  Producers
-   openssl ca -batch -days 364 -cert cacert.pem -keyfile caprivkey.pem \
-   -in csrRiv13606RequestEHRExtractProducer.pem -out Riv13606RequestEHRExtractProducer-ca-cert.pem
+   openssl ca -batch -days 7200 -cert cacert.pem -keyfile caprivkey.pem \
+   -in csrProducer.pem -out Producer-ca-cert.pem
+
+   openssl ca -batch -days 7200 -cert cacert.pem -keyfile caprivkey.pem \
+   -in csrProducerLocalhost.pem -out ProducerLocalhost-ca-cert.pem
 
 # Rewrite the certificates in PEM only format. This allows us to concatenate
 # them into chains.
-    openssl x509 -in Riv13606RequestEHRExtractConsumer-ca-cert.pem -out Riv13606RequestEHRExtractConsumer-ca-cert.pem -outform PEM
-    openssl x509 -in Riv13606RequestEHRExtractProducer-ca-cert.pem -out Riv13606RequestEHRExtractProducer-ca-cert.pem -outform PEM
+    openssl x509 -in Consumer-ca-cert.pem -out Consumer-ca-cert.pem -outform PEM
+    openssl x509 -in Producer-ca-cert.pem -out Producer-ca-cert.pem -outform PEM
+    openssl x509 -in ProducerLocalhost-ca-cert.pem -out ProducerLocalhost-ca-cert.pem -outform PEM
 
 # Create a chain readable by CertificateFactory.getCertificates.
-    cat Riv13606RequestEHRExtractConsumer-ca-cert.pem cacert.pem > Riv13606RequestEHRExtractConsumer.chain
-    cat Riv13606RequestEHRExtractProducer-ca-cert.pem cacert.pem > Riv13606RequestEHRExtractProducer.chain
+    cat Consumer-ca-cert.pem cacert.pem > Consumer.chain
+    cat Producer-ca-cert.pem cacert.pem > Producer.chain
+    cat ProducerLocalhost-ca-cert.pem cacert.pem > ProducerLocalhost.chain
 
-# Replace the certificates in the tp, client and service keystores with their respective
+# Replace the certificates in the client and service keystores with their respective
 # full chains.
-    keytool -import -file Riv13606RequestEHRExtractConsumer.chain -keystore consumer.jks -storetype jks \
-    -alias Riv13606RequestEHRExtractConsumer \
+    keytool -import -file Consumer.chain -keystore consumer.jks -storetype jks \
+    -alias Consumer \
     -storepass password -keypass password -noprompt
 
-    keytool -import -file Riv13606RequestEHRExtractProducer.chain -keystore producer.jks -storetype jks \
-    -alias Riv13606RequestEHRExtractProducer \
+    keytool -import -file Producer.chain -keystore producer.jks -storetype jks \
+    -alias Producer \
+    -storepass password -keypass password -noprompt
+
+    keytool -import -file ProducerLocalhost.chain -keystore producer-localhost.jks -storetype jks \
+    -alias ProducerLocalhost \
     -storepass password -keypass password -noprompt
 
 # Create the Truststore file containing the CA cert.
     keytool -import -file cacert.pem -alias TheCA -keystore truststore.jks \
     -storepass password -noprompt
-    keytool -import -file cacert.pem -alias TheCA -keystore consumer-truststore.jks -storepass password -noprompt
-    keytool -import -file cacert.pem -alias TheCA -keystore producer-truststore.jks -storepass password -noprompt
 
 # List keystores for manual inspection
     keytool -v -list -keystore consumer.jks -storepass password > consumer.jks.log
     keytool -v -list -keystore producer.jks -storepass password > producer.jks.log
+    keytool -v -list -keystore producer-localhost.jks -storepass password > producerlocalhost.jks.log
     keytool -v -list -keystore truststore.jks -storepass password > truststore.jks.log
 
