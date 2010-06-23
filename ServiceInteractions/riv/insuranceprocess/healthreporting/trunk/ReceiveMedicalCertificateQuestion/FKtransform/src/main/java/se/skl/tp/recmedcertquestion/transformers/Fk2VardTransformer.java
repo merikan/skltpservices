c28606e4df638e8354fdc30210ddaeaac57828b6
matsek <matsek@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2010-06-23 12:56:34 +0000

    Added receiveMedicalCertificateQuestion project

---
+++ ServiceInteractions/riv/insuranceprocess/healthreporting/trunk/ReceiveMedicalCertificateQuestion/FKtransform/src/main/java/se/skl/tp/recmedcertquestion/transformers/Fk2VardTransformer.java
@@ -0,0 +1,124 @@
+package se.skl.tp.recmedcertquestion.transformers;
+
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+
+import org.mule.api.ExceptionPayload;
+import org.mule.api.MuleMessage;
+import org.mule.api.routing.ResponseTimeoutException;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageAwareTransformer;
+import org.mule.transport.NullPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.fk.vardgivare.sjukvard.taemotlakarintygresponder.v1.TaEmotLakarintygResponseType;
+import se.skl.riv.insuranceprocess.healthreporting.registermedicalcertificateresponder.v2.RegisterMedicalCertificateResponseType;
+import se.skl.riv.insuranceprocess.healthreporting.v1.ResultCodeEnum;
+import se.skl.riv.insuranceprocess.healthreporting.v1.ResultOfCall;
+
+public class Fk2VardTransformer extends AbstractMessageAwareTransformer
+{
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	public Fk2VardTransformer()
+    {
+        super();
+        registerSourceType(Object.class);
+        setReturnClass(Object.class);
+    }
+    
+	public Object transform(MuleMessage message, String outputEncoding) throws TransformerException {
+
+		boolean faultDetected = false;
+		
+    	Object src = message.getPayload();
+		try {						
+			// Take care of any error message and send it back as a SOAP Fault!
+			if (src instanceof NullPayload) {
+			    // We got a null-payload, let's see if there is an exception-payload instead...
+		        ExceptionPayload ep = message.getExceptionPayload();
+		        if (ep != null) {
+		            String errorMessage = ep.getCode() + ": " + ep.getMessage();
+
+		            Throwable t = ep.getException();
+		            if (t instanceof ResponseTimeoutException) {
+		            	src = errorMessage + ". Unknown error.";
+		            } else {
+		                src = errorMessage + ". Timeout";
+		            }
+		            
+		            // Remove the ExceptionPayload!
+		            message.setExceptionPayload(null);
+		        }				
+	            faultDetected = true;
+			} else if(!(src instanceof TaEmotLakarintygResponseType)) {
+				src = "Payload type not supported: "+ message.getPayload().getClass();
+				faultDetected = true;
+			}
+
+//			QName name = getRootElementQName(src);
+
+			StringBuffer result = new StringBuffer();
+			
+			// First create the content in the body, either a fault or the response
+			if (faultDetected) {
+				// Strip off xml processing instructions if any
+				String payload = (String)src;
+				if (payload.startsWith("<?")) {
+					int pos = payload.indexOf("?>");
+					payload = payload.substring(pos + 2);
+				}
+
+				createSoapFault(payload, result);
+			} else {
+	            TaEmotLakarintygResponseType inResponse = (TaEmotLakarintygResponseType)src;
+
+	            // Create new JAXB object for the outgoing data
+	            RegisterMedicalCertificateResponseType outResponse = new RegisterMedicalCertificateResponseType();
+	            
+	            ResultOfCall resultOfCall = new ResultOfCall();
+	            
+	            // Check result
+	            if (inResponse != null) {
+	            	resultOfCall.setResultCode(ResultCodeEnum.OK);
+	            	outResponse.setResult(resultOfCall);
+	            }
+	            
+	            // If payload already is a SoapFault How to use marshalling?
+	            
+				// Transform the JAXB object into a XML payload
+	            StringWriter writer = new StringWriter();
+	        	Marshaller marshaller = JAXBContext.newInstance(RegisterMedicalCertificateResponseType.class).createMarshaller();
+	        	marshaller.marshal(new JAXBElement(new QName("urn:riv:insuranceprocess:healthreporting:RegisterMedicalCertificateResponder:1", "RegisterMedicalCertificateResponse"), RegisterMedicalCertificateResponseType.class, outResponse), writer);
+				logger.debug("Extracted information: {}", writer.toString());
+				String payload = (String)writer.toString();
+				if (payload.startsWith("<?")) {
+					int pos = payload.indexOf("?>");
+					payload = payload.substring(pos + 2);
+				}
+
+				result.append(payload);
+			}
+
+			// Done, return the string
+			String resultStr = result.toString();
+			logger.debug("Return SOAP Envelope: {}", resultStr);
+			return resultStr;
+
+		} catch (Exception e) {
+	        throw new TransformerException(this, e);
+		}
+	}
+			
+	private void createSoapFault(String errorText, StringBuffer result) {
+		result.append("<soap:Fault>");
+		result.append("<faultcode>soap:Server</faultcode>");
+		result.append("<faultstring>VP009 Exception when calling the service producer: " + errorText + "</faultstring>");
+		result.append("</soap:Fault>");
+	}
+}
---
+++ ServiceInteractions/riv/insuranceprocess/healthreporting/trunk/ReceiveMedicalCertificateQuestion/FKtransform/src/main/java/se/skl/tp/recmedcertquestion/transformers/Vard2FkTransformer.java
@@ -0,0 +1,90 @@
+package se.skl.tp.recmedcertquestion.transformers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.ResourceBundle;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageAwareTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3.wsaddressing10.AttributedURIType;
+
+import se.skl.riv.insuranceprocess.healthreporting.receivemedicalcertificateanswerresponder.v1.ReceiveMedicalCertificateAnswerType;
+
+public class Vard2FkTransformer extends AbstractMessageAwareTransformer
+{
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	public Vard2FkTransformer()
+    {
+        super();
+        registerSourceType(Object.class);
+        setReturnClass(Object.class); 
+    }
+    
+	public Object transform(MuleMessage message, String outputEncoding) throws TransformerException {
+		ResourceBundle rb = ResourceBundle.getBundle("fkdata");	    
+
+		try {			
+			// Transform the XML payload into a JAXB object
+            Unmarshaller unmarshaller = JAXBContext.newInstance(ReceiveMedicalCertificateAnswerType.class).createUnmarshaller();
+            XMLStreamReader streamPayload = (XMLStreamReader)((Object[])message.getPayload())[1];
+            ReceiveMedicalCertificateAnswerType inRequest = (ReceiveMedicalCertificateAnswerType)((JAXBElement)unmarshaller.unmarshal(streamPayload)).getValue();
+		
+			// Get receiver to adress from Mule property
+			String receiverId = (String)message.getProperty("receiverid");
+			
+
+    		AttributedURIType logicalAddressHeader = new AttributedURIType();
+    		logicalAddressHeader.setValue(receiverId);
+
+// OBS !! ej null skall vara transformerad request
+    		Object[] payloadOut = new Object[] {logicalAddressHeader, null};
+            
+	        if (logger.isDebugEnabled()) {
+	            logger.debug("transformed payload to: " + payloadOut);
+	        }
+
+	        return payloadOut;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
+		
+	private XMLGregorianCalendar getDate(String stringDate) throws Exception{
+
+		try {
+			GregorianCalendar fromDate = new GregorianCalendar();
+			DateFormat dfm = new SimpleDateFormat("yyyyMMdd");
+			Date date = dfm.parse(stringDate);
+			fromDate.setTime(date);
+			return (DatatypeFactory.newInstance().newXMLGregorianCalendar(fromDate));
+		} catch (DatatypeConfigurationException e) {
+			throw new Exception(e.getMessage());
+		} catch (ParseException pe) {
+			throw new Exception(pe.getMessage());
+		}
+	}
+	
+}
