4f265780fc9cfdea432a16f655d7843fcfd099c7
matsek <matsek@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2010-07-05 09:09:48 +0000

    Added project ReceiveMedicalCertificateAnswer

---
+++ ServiceInteractions/riv/insuranceprocess/healthreporting/trunk/ReceiveMedicalCertificateAnswer/Test-producer/src/main/java/se/skl/tp/insuranceprocess/healthreporting/recmedcertanswer/producer/RecMedCertAnswerImpl.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2009 Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+
+ *   Boston, MA 02111-1307  USA
+ */
+package se.skl.tp.insuranceprocess.healthreporting.recmedcertanswer.producer;
+
+import javax.jws.WebService;
+
+import org.w3.wsaddressing10.AttributedURIType;
+
+import se.skl.riv.insuranceprocess.healthreporting.receivemedicalcertificateanswer.v1.rivtabp20.ReceiveMedicalCertificateAnswerResponderInterface;
+import se.skl.riv.insuranceprocess.healthreporting.receivemedicalcertificateanswerresponder.v1.ReceiveMedicalCertificateAnswerResponseType;
+import se.skl.riv.insuranceprocess.healthreporting.receivemedicalcertificateanswerresponder.v1.ReceiveMedicalCertificateAnswerType;
+import se.skl.riv.insuranceprocess.healthreporting.v1.ResultCodeEnum;
+import se.skl.riv.insuranceprocess.healthreporting.v1.ResultOfCall;
+
+
+@WebService(
+		serviceName = "ReceiveMedicalCertificateAnswerResponderService", 
+		endpointInterface="se.skl.riv.insuranceprocess.healthreporting.receivemedicalcertificateanswer.v1.rivtabp20.ReceiveMedicalCertificateAnswerResponderInterface", 
+		portName = "ReceiveMedicalCertificateAnswerResponderPort", 
+		targetNamespace = "urn:riv:insuranceprocess:healthreporting:ReceiveMedicalCertificateAnswer:1:rivtabp20",
+		wsdlLocation = "schemas/ReceiveMedicalCertificateAnswerInteraction_0.9_rivtabp20.wsdl")
+public class RecMedCertAnswerImpl implements ReceiveMedicalCertificateAnswerResponderInterface {
+
+	public ReceiveMedicalCertificateAnswerResponseType receiveMedicalCertificateAnswer(
+			AttributedURIType logicalAddress,
+			ReceiveMedicalCertificateAnswerType parameters) {
+		try {
+			ReceiveMedicalCertificateAnswerResponseType response = new ReceiveMedicalCertificateAnswerResponseType();
+			
+			// Ping response
+			ResultOfCall resCall = new ResultOfCall();
+			resCall.setResultCode(ResultCodeEnum.OK);
+			response.setResult(resCall);
+
+			return response;
+		} catch (RuntimeException e) {
+			System.out.println("Error occured: " + e);
+			throw e;
+		}
+	}
+}
