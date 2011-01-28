630e593bcaa3889487f74791fbfda2b5e273de61
matsek <matsek@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2011-01-28 13:43:50 +0000

    Housecleaning

--- ServiceInteractions/riv/insuranceprocess/healthreporting/trunk/SendMedicalCertificateAnswer/Test-producer/src/main/java/se/skl/tp/insuranceprocess/healthreporting/sendmedcertanswer/producer/SendMedCertAnswerProducer.java
+++ ServiceInteractions/riv/insuranceprocess/healthreporting/trunk/SendMedicalCertificateAnswer/Test-producer/src/main/java/se/skl/tp/insuranceprocess/healthreporting/sendmedcertanswer/producer/SendMedCertAnswerProducer.java
@@ -21,13 +21,24 @@
 
 package se.skl.tp.insuranceprocess.healthreporting.sendmedcertanswer.producer;
 
+import java.net.URL;
+
 import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBusFactory;
 
 
 public class SendMedCertAnswerProducer {
 
     protected SendMedCertAnswerProducer() throws Exception {
         System.out.println("Starting Producer");
+
+        // Loads a cxf configuration file to use
+        SpringBusFactory bf = new SpringBusFactory();
+        URL busFile = ClassLoader.getSystemResource("cxf-producer.xml");
+        Bus bus = bf.createBus(busFile.toString());
+        SpringBusFactory.setDefaultBus(bus);
 
         Object implementor = new SendMedCertAnswerImpl();
         String address = "https://localhost:19000/vard/SendMedicalCertificateAnswer/1/rivtabp20";
