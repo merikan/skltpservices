6fa36007cd076c9929667357197b00623c4e91b1
matsek <matsek@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2011-06-03 07:47:16 +0000

    Creating a release branch

---
+++ ServiceInteractions/riv/insuranceprocess/healthreporting/branches/TD_IFV_1_0_RB/RegisterMedicalCertificate/Test-producer/src/main/java/se/skl/tp/insuranceprocess/healthreporting/regmedcert/producer/RegisterMedCertImpl.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2009 Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+
+ *   Boston, MA 02111-1307  USA
+ */
+package se.skl.tp.insuranceprocess.healthreporting.regmedcert.producer;
+
+import javax.jws.WebService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3.wsaddressing10.AttributedURIType;
+
+import se.skl.riv.insuranceprocess.healthreporting.registermedicalcertificate.v3.rivtabp20.RegisterMedicalCertificateResponderInterface;
+import se.skl.riv.insuranceprocess.healthreporting.registermedicalcertificateresponder.v3.RegisterMedicalCertificateResponseType;
+import se.skl.riv.insuranceprocess.healthreporting.registermedicalcertificateresponder.v3.RegisterMedicalCertificateType;
+import se.skl.riv.insuranceprocess.healthreporting.v2.ResultCodeEnum;
+import se.skl.riv.insuranceprocess.healthreporting.v2.ResultOfCall;
+
+
+@WebService(
+		serviceName = "RegisterMedicalCertificateResponderService", 
+		endpointInterface="se.skl.riv.insuranceprocess.healthreporting.registermedicalcertificate.v3.rivtabp20.RegisterMedicalCertificateResponderInterface", 
+		portName = "RegisterMedicalCertificateResponderPort", 
+		targetNamespace = "urn:riv:insuranceprocess:healthreporting:RegisterMedicalCertificate:3:rivtabp20",
+		wsdlLocation = "schemas/vard/interactions/RegisterMedicalCertificateInteraction/RegisterMedicalCertificateInteraction_3.0_rivtabp20.wsdl")
+public class RegisterMedCertImpl implements RegisterMedicalCertificateResponderInterface {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	public RegisterMedicalCertificateResponseType registerMedicalCertificate(
+			AttributedURIType logicalAddress,
+			RegisterMedicalCertificateType parameters) {
+		try {
+			logger.debug("Received call not validating!");
+			
+			RegisterMedicalCertificateResponseType response = new RegisterMedicalCertificateResponseType();
+			
+			// Ping response
+			ResultOfCall resCall = new ResultOfCall();
+			resCall.setResultCode(ResultCodeEnum.OK);
+			response.setResult(resCall);
+			
+			logger.debug("Returned OK for not validating!");
+
+			return response;
+		} catch (RuntimeException e) {
+			System.out.println("Error occured: " + e);
+			throw e;
+		}
+	}
+}
