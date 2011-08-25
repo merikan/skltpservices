7afd26ae7f3b887314ed27f03efbbd642aa0d32e
matsek <matsek@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2011-08-25 12:45:33 +0000

    Added error handling cases to testproducer!

--- ServiceInteractions/riv/insuranceprocess/healthreporting/trunk/RegisterMedicalCertificate/Test-producer/src/main/java/se/skl/tp/insuranceprocess/healthreporting/regmedcert/producer/RegisterMedCertImpl.java
+++ ServiceInteractions/riv/insuranceprocess/healthreporting/trunk/RegisterMedicalCertificate/Test-producer/src/main/java/se/skl/tp/insuranceprocess/healthreporting/regmedcert/producer/RegisterMedCertImpl.java
@@ -48,19 +48,49 @@ public class RegisterMedCertImpl implements RegisterMedicalCertificateResponderI
 			RegisterMedicalCertificateType parameters) {
 		try {
 			logger.debug("Received call not validating!");
-			
 			RegisterMedicalCertificateResponseType response = new RegisterMedicalCertificateResponseType();
+						
+			String logiskAdress = logicalAddress.getValue();
 			
-			// Ping response
-			ResultOfCall resCall = new ResultOfCall();
-			resCall.setResultCode(ResultCodeEnum.OK);
-			response.setResult(resCall);
-			
-			logger.debug("Returned OK for not validating!");
+			// Check if we should throw some kind of exception or simulate a timeout.
+			if (	parameters != null && parameters.getLakarutlatande() != null && 
+					parameters.getLakarutlatande().getPatient() != null && 
+					parameters.getLakarutlatande().getPatient().getFullstandigtNamn() != null &&
+					parameters.getLakarutlatande().getPatient().getFullstandigtNamn().length() > 0) {
 
+				String name = parameters.getLakarutlatande().getPatient().getFullstandigtNamn();
+				
+				if (name.equalsIgnoreCase("Error") || logiskAdress.contains("Error") ) {
+					ResultOfCall result = new ResultOfCall();
+					result.setResultCode(ResultCodeEnum.ERROR);
+					response.setResult(result);
+					logger.debug("Returned Error for not validating!");
+				} else if (name.equalsIgnoreCase("Exception") || logiskAdress.contains("Exception")) {
+					logger.debug("Returned Exception for not validating!");
+					throw new RuntimeException("Exception called");
+				} else if (name.equalsIgnoreCase("Timeout") || logiskAdress.contains("Timeout")) {
+					logger.debug("Returned Timeout for not validating!");
+					Thread.currentThread();
+					try {
+						Thread.sleep(60000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				} else {
+					ResultOfCall resCall = new ResultOfCall();
+					resCall.setResultCode(ResultCodeEnum.OK);
+					response.setResult(resCall);				
+					logger.debug("Returned OK for not validating!");
+				}
+			} else {
+				ResultOfCall resCall = new ResultOfCall();
+				resCall.setResultCode(ResultCodeEnum.OK);
+				response.setResult(resCall);				
+				logger.debug("Returned OK for not validating!");
+			}
+			
 			return response;
 		} catch (RuntimeException e) {
-			System.out.println("Error occured: " + e);
 			throw e;
 		}
 	}
