8b35d048bff04be75e85241845c28e17481a0f07
matsek <matsek@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2011-09-06 07:52:08 +0000

    New certs

--- ServiceInteractions/riv/crm/carelisting/trunk/GetListing/Test-producer/src/main/java/se/skl/tp/crm/carelisting/getlisting/producer/GetListingProducer.java
+++ ServiceInteractions/riv/crm/carelisting/trunk/GetListing/Test-producer/src/main/java/se/skl/tp/crm/carelisting/getlisting/producer/GetListingProducer.java
@@ -21,12 +21,24 @@
 
 package se.skl.tp.crm.carelisting.getlisting.producer;
 
+import java.net.URL;
+
 import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBusFactory;
 
 public class GetListingProducer {
 
     protected GetListingProducer() throws Exception {
         System.out.println("Starting Producer");
+
+        // Loads a cxf configuration file to use
+        final SpringBusFactory bf = new SpringBusFactory();
+        final URL busFile = this.getClass().getClassLoader().getResource("cxf-producer.xml");
+        final Bus bus = bf.createBus(busFile.toString());
+        
+        SpringBusFactory.setDefaultBus(bus);
 
         Object implementor = new GetListingImpl();
         String address = "https://localhost:19000/vp/GetListing/1/rivtabp20";
