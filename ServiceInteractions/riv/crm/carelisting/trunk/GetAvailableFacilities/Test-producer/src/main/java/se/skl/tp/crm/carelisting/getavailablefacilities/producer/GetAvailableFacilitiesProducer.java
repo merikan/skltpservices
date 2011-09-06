7487e99a0d117fb4ff31f56b3a028471f377130f
matsek <matsek@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2011-09-06 07:25:24 +0000

    We will now read a specified cxf configuration file

--- ServiceInteractions/riv/crm/carelisting/trunk/GetAvailableFacilities/Test-producer/src/main/java/se/skl/tp/crm/carelisting/getavailablefacilities/producer/GetAvailableFacilitiesProducer.java
+++ ServiceInteractions/riv/crm/carelisting/trunk/GetAvailableFacilities/Test-producer/src/main/java/se/skl/tp/crm/carelisting/getavailablefacilities/producer/GetAvailableFacilitiesProducer.java
@@ -21,12 +21,24 @@
 
 package se.skl.tp.crm.carelisting.getavailablefacilities.producer;
 
+import java.net.URL;
+
 import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBusFactory;
 
 public class GetAvailableFacilitiesProducer {
 
     protected GetAvailableFacilitiesProducer() throws Exception {
         System.out.println("Starting Producer");
+
+        // Loads a cxf configuration file to use
+        final SpringBusFactory bf = new SpringBusFactory();
+        final URL busFile = this.getClass().getClassLoader().getResource("cxf-producer.xml");
+        final Bus bus = bf.createBus(busFile.toString());
+        
+        SpringBusFactory.setDefaultBus(bus);
 
         Object implementor = new GetAvailableFacilitiesImpl();
         String address = "https://localhost:19000/vp/GetAvailableFacilities/1/rivtabp20";
