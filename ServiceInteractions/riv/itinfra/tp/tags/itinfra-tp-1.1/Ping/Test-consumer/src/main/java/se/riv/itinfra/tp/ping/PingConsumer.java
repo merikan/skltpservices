536d18b5cf83ba2c764e9206558429fc94c56817
hthunberg <hthunberg@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2011-09-07 12:46:24 +0000

    [maven-release-plugin]  copy for tag itinfra-tp-1.1

---
+++ ServiceInteractions/riv/itinfra/tp/tags/itinfra-tp-1.1/Ping/Test-consumer/src/main/java/se/riv/itinfra/tp/ping/PingConsumer.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2009 Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+
+ *   Boston, MA 02111-1307  USA
+ */
+package se.riv.itinfra.tp.ping;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.w3.wsaddressing10.AttributedURIType;
+
+import se.riv.itinfra.tp.ping.v1.rivtabp20.PingResponderInterface;
+import se.riv.itinfra.tp.ping.v1.rivtabp20.PingResponderService;
+import se.riv.itinfra.tp.pingresponder.v1.PingRequestType;
+import se.riv.itinfra.tp.pingresponder.v1.PingResponseType;
+
+public final class PingConsumer {
+
+	// Use this one to connect via Virtualiseringsplattformen
+	private static final String LOGISK_ADDRESS = "/Ping_Service";
+	// Use this one to connect directly (just for test)
+
+	public static void main(String[] args) {
+		String host = "localhost:10000/test";
+		if (args.length > 0) {
+			host = args[0];
+		}
+
+		// Setup ssl info for the initial ?wsdl lookup...
+		System.setProperty("javax.net.ssl.keyStore","../../certs/consumer.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "password");
+		System.setProperty("javax.net.ssl.keyStoreType", "jks");
+		System.setProperty("javax.net.ssl.trustStore", "../../certs/truststore.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "password");
+
+		String adress = "https://" + host + LOGISK_ADDRESS;
+		System.out.println("Consumer connecting to "  + adress);
+		String p = callService(adress, "Test");
+		System.out.println("Returned: " + p);
+	}
+
+	public static String callService(String serviceAddress, String logicalAddresss) {
+		
+		PingResponderInterface service = new PingResponderService(
+			createEndpointUrlFromServiceAddress(serviceAddress)).getPingResponderPort();
+
+		AttributedURIType logicalAddressHeader = new AttributedURIType();
+		logicalAddressHeader.setValue(logicalAddresss);
+
+		PingRequestType request = new PingRequestType();
+		
+		// The below request is created to mimic the sample xml-message
+		request.setPingIn("Pinging!");
+	
+		try {
+		    PingResponseType result = service.ping(logicalAddressHeader, request);
+
+		    return ("Ping response=" + result.getPingUt());
+
+		} catch (Exception ex) {
+			System.out.println("Exception=" + ex.getMessage());
+			return null;
+		}
+	}
+
+	public static URL createEndpointUrlFromServiceAddress(String serviceAddress) {
+		try {
+			return new URL(serviceAddress + "?wsdl");
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("Malformed URL Exception: " + e.getMessage());
+		}
+	}
+}
