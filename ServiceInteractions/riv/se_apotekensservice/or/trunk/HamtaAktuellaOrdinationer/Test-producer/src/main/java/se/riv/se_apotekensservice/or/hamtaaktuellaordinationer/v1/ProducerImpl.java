aff5cc782847c42c9b377c7d411401660650609a
hthunberg <hthunberg@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2011-10-03 08:57:39 +0000

    Organized imports, updated test producer url

--- ServiceInteractions/riv/se_apotekensservice/or/trunk/HamtaAktuellaOrdinationer/Test-producer/src/main/java/se/riv/se_apotekensservice/or/hamtaaktuellaordinationer/v1/ProducerImpl.java
+++ ServiceInteractions/riv/se_apotekensservice/or/trunk/HamtaAktuellaOrdinationer/Test-producer/src/main/java/se/riv/se_apotekensservice/or/hamtaaktuellaordinationer/v1/ProducerImpl.java
@@ -22,24 +22,23 @@ package se.riv.se_apotekensservice.or.hamtaaktuellaordinationer.v1;
 
 import javax.jws.WebService;
 
-import org.w3.wsaddressing10.AttributedURIType;
-
-import riv.inera.se_apotekensservice.or.hamtaaktuellaordinationer._1.rivtabp20.ApplicationException;
-import riv.inera.se_apotekensservice.or.hamtaaktuellaordinationer._1.rivtabp20.HamtaAktuellaOrdinationerResponderInterface;
-import riv.inera.se_apotekensservice.or.hamtaaktuellaordinationer._1.rivtabp20.SystemException;
-import riv.inera_se_apotekensservice.argos._1.ArgosHeaderType;
-import riv.se_apotekensservice.or.hamtaaktuellaordinationerresponder._1.HamtaAktuellaOrdinationerRequestType;
-import riv.se_apotekensservice.or.hamtaaktuellaordinationerresponder._1.HamtaAktuellaOrdinationerResponseType;
+import se.riv.inera.se.apotekensservice.argos.v1.ArgosHeaderType;
+import se.riv.inera.se.apotekensservice.or.hamtaaktuellaordinationer.v1.rivtabp20.HamtaAktuellaOrdinationerResponderInterface;
+import se.riv.se.apotekensservice.or.hamtaaktuellaordinationerresponder.v1.HamtaAktuellaOrdinationerRequestType;
+import se.riv.se.apotekensservice.or.hamtaaktuellaordinationerresponder.v1.HamtaAktuellaOrdinationerResponseType;
 
 @WebService(serviceName = "HamtaAktuellaOrdinationerResponderService", endpointInterface = "riv.inera.se_apotekensservice.or.hamtaaktuellaordinationer._1.rivtabp20.HamtaAktuellaOrdinationerResponderInterface", portName = "HamtaAktuellaOrdinationerResponderPort", targetNamespace = "urn:riv:inera:se.apotekensservice:or:HamtaAktuellaOrdinationer:1:rivtabp20", wsdlLocation = "schemas/interactions/HamtaAktuellaOrdinationerInteraction/HamtaAktuellaOrdinationerInteraction_1.0_rivtabp20.wsdl")
 public class ProducerImpl implements HamtaAktuellaOrdinationerResponderInterface {
 
     @Override
     public HamtaAktuellaOrdinationerResponseType hamtaAktuellaOrdinationer(
-	    HamtaAktuellaOrdinationerRequestType parameters, AttributedURIType logicalAddress,
-	    ArgosHeaderType argosHeader) throws ApplicationException, SystemException {
+	    HamtaAktuellaOrdinationerRequestType parameters, org.w3c.addressing.v1.AttributedURIType logicalAddress,
+	    ArgosHeaderType argosHeader)
+	    throws se.riv.inera.se.apotekensservice.or.hamtaaktuellaordinationer.v1.rivtabp20.ApplicationException,
+	    se.riv.inera.se.apotekensservice.or.hamtaaktuellaordinationer.v1.rivtabp20.SystemException {
 
 	HamtaAktuellaOrdinationerResponseType response = new HamtaAktuellaOrdinationerResponseType();
 	return response;
     }
+
 }
