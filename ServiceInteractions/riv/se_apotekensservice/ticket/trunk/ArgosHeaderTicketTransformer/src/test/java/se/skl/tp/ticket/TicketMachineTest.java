eb3538eb1a16536c8f1233d667fd3d675c8c58b3
hthunberg <hthunberg@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2011-10-10 08:39:43 +0000

    Updated testdata to more realistic values

--- ServiceInteractions/riv/se_apotekensservice/ticket/trunk/ArgosHeaderTicketTransformer/src/test/java/se/skl/tp/ticket/TicketMachineTest.java
+++ ServiceInteractions/riv/se_apotekensservice/ticket/trunk/ArgosHeaderTicketTransformer/src/test/java/se/skl/tp/ticket/TicketMachineTest.java
@@ -13,27 +13,27 @@ public class TicketMachineTest {
     @Test
     public void testProduceCompleteSamlTicketFromArgosHeader() throws TicketMachineException {
 
-	String forskrivarkod = "1111129";
-	String legitimationskod = "";
+	String forskrivarkod = "1111152";
+	String legitimationskod = "1111111";
 	String fornamn = "Lars";
 	String efternamn = "LŠkare";
-	String yrkesgrupp = "LŠkare";
-	String befattningskod = "123456";
-	String arbetsplatskod = "1234567890";
-	String arbetsplatsnamn = "Sjukhuset";
+	String yrkesgrupp = "FORSKRIVARE";
+	String befattningskod = "1111111";
+	String arbetsplatskod = "4000000000001";
+	String arbetsplatsnamn = "VC Test";
 	String postort = "Staden";
 	String postadress = "VŠgen 1";
 	String postnummer = "11111";
-	String telefonnummer = "08-1234567";
-	String requestId = "123456";
-	String rollnamn = "FORSKRIVARE";
-	String directoryID = "TSE6565656565-1003";
-	String hsaID = "TSE6565656565-1003";
+	String telefonnummer = "0987654321";
+	String requestId = "12345676";
+	String rollnamn = "LK";
+	String directoryID = "SE1111111111-1003";
+	String hsaID = "SE1111111111-1003";
 	String katalog = "HSA";
-	String organisationsnummer = "1234567890";
-	String systemnamn = "Melior";
+	String organisationsnummer = "111111111";
+	String systemnamn = "Pascal";
 	String systemversion = "1.0";
-	String systemIp = "192.0.0.1";
+	String systemIp = "192.168.1.1";
 
 	ArgosHeader argosHeader = new ArgosHeader(forskrivarkod, legitimationskod, fornamn, efternamn, yrkesgrupp,
 		befattningskod, arbetsplatskod, arbetsplatsnamn, postort, postadress, postnummer, telefonnummer,
@@ -41,6 +41,7 @@ public class TicketMachineTest {
 		systemIp);
 
 	String ticket = new TicketMachine().produceSamlTicket(argosHeader);
+	
 
 	Assert.assertThat(ticket, containsString("<saml2:Issuer>pascalonline</saml2:Issuer>"));
 
