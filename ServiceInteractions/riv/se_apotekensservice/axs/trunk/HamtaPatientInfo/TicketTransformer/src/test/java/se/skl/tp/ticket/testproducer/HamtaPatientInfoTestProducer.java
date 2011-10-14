ff6df6c3ef885799cdfa8443c40b2a901973dd9d
hthunberg <hthunberg@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2011-10-14 12:34:25 +0000

    Added encoding test

--- ServiceInteractions/riv/se_apotekensservice/axs/trunk/HamtaPatientInfo/TicketTransformer/src/test/java/se/skl/tp/ticket/testproducer/HamtaPatientInfoTestProducer.java
+++ ServiceInteractions/riv/se_apotekensservice/axs/trunk/HamtaPatientInfo/TicketTransformer/src/test/java/se/skl/tp/ticket/testproducer/HamtaPatientInfoTestProducer.java
@@ -4,6 +4,8 @@ import javax.jws.WebService;
 
 import org.w3c.addressing.v1.AttributedURIType;
 
+import riv.se_apotekensservice.axs._1.ApoteksinformationResponse;
+import riv.se_apotekensservice.axs._1.PatientInformationResponse;
 import se.riv.inera.se.apotekensservice.argos.v1.ArgosHeaderType;
 import se.riv.inera.se.apotekensservice.axs.hamtapatientinfo.v1.rivtabp20.ApplicationException;
 import se.riv.inera.se.apotekensservice.axs.hamtapatientinfo.v1.rivtabp20.HamtaPatientInfoResponderInterface;
@@ -18,8 +20,25 @@ public class HamtaPatientInfoTestProducer implements HamtaPatientInfoResponderIn
     public HamtaPatientInfoResponseType hamtaPatientInfo(HamtaPatientInfoRequestType arg0, AttributedURIType arg1,
 	    ArgosHeaderType arg2) throws ApplicationException, SystemException {
 	HamtaPatientInfoResponseType response = new HamtaPatientInfoResponseType();
+	String ssn = arg0.getPersonnummer();
+	
+	response.setApotek(createApoteksInformationResponse(ssn));
+	response.setPatientInformation(createPatientInformationResponse());
 	response.setFinnsOrdination(true);
+	response.setDosproducent(ssn);
 	return response;
+    }
+
+    private ApoteksinformationResponse createApoteksInformationResponse(String ssn) {
+	ApoteksinformationResponse apoteksinformationResponse = new ApoteksinformationResponse();
+	apoteksinformationResponse.setAktorsnamn("Aktörsnamn");
+	return null;
+    }
+
+    private PatientInformationResponse createPatientInformationResponse() {
+	PatientInformationResponse informationResponse = new PatientInformationResponse();
+	informationResponse.setOmradeskod("Områdeskod");
+	return null;
     }
 
 }
