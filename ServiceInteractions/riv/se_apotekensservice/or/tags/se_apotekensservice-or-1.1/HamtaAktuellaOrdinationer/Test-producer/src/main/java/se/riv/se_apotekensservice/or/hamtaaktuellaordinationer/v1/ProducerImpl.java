e600773369be1e705de93832eade290ee73300ff
hans.thunberg <hans.thunberg@callistaenterprise.se>
2012-01-02 13:43:35 +0000

    [maven-release-plugin]  copy for tag se_apotekensservice-or-1.1

---
+++ ServiceInteractions/riv/se_apotekensservice/or/tags/se_apotekensservice-or-1.1/HamtaAktuellaOrdinationer/Test-producer/src/main/java/se/riv/se_apotekensservice/or/hamtaaktuellaordinationer/v1/ProducerImpl.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2009 Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+
+ *   Boston, MA 02111-1307  USA
+ */
+package se.riv.se_apotekensservice.or.hamtaaktuellaordinationer.v1;
+
+import javax.jws.WebService;
+
+import riv.se_apotekensservice.or._1.DosunderlagResponse;
+import riv.se_apotekensservice.or._1.OrdinationslistaResponse;
+import se.riv.inera.se.apotekensservice.argos.v1.ArgosHeaderType;
+import se.riv.inera.se.apotekensservice.or.hamtaaktuellaordinationer.v1.rivtabp20.HamtaAktuellaOrdinationerResponderInterface;
+import se.riv.se.apotekensservice.or.hamtaaktuellaordinationerresponder.v1.HamtaAktuellaOrdinationerRequestType;
+import se.riv.se.apotekensservice.or.hamtaaktuellaordinationerresponder.v1.HamtaAktuellaOrdinationerResponseType;
+
+@WebService(serviceName = "HamtaAktuellaOrdinationerResponderService", endpointInterface = "se.riv.inera.se.apotekensservice.or.hamtaaktuellaordinationer.v1.rivtabp20.HamtaAktuellaOrdinationerResponderInterface", portName = "HamtaAktuellaOrdinationerResponderPort", targetNamespace = "urn:riv:inera:se.apotekensservice:or:HamtaAktuellaOrdinationer:1:rivtabp20", wsdlLocation = "schemas/interactions/HamtaAktuellaOrdinationerInteraction/HamtaAktuellaOrdinationerInteraction_1.0_rivtabp20.wsdl")
+public class ProducerImpl implements HamtaAktuellaOrdinationerResponderInterface {
+
+    @Override
+    public HamtaAktuellaOrdinationerResponseType hamtaAktuellaOrdinationer(
+	    HamtaAktuellaOrdinationerRequestType parameters, org.w3c.addressing.v1.AttributedURIType logicalAddress,
+	    ArgosHeaderType argosHeader)
+	    throws se.riv.inera.se.apotekensservice.or.hamtaaktuellaordinationer.v1.rivtabp20.ApplicationException,
+	    se.riv.inera.se.apotekensservice.or.hamtaaktuellaordinationer.v1.rivtabp20.SystemException {
+
+	HamtaAktuellaOrdinationerResponseType response = new HamtaAktuellaOrdinationerResponseType();
+	response.setOrdinationslista(createOrdinationsLista(parameters.getPersonnummer()));
+	return response;
+    }
+
+    private OrdinationslistaResponse createOrdinationsLista(String personnummer) {
+	OrdinationslistaResponse ordinationslistaResponse = new OrdinationslistaResponse();
+	ordinationslistaResponse.setPersonnummer(personnummer);
+
+	DosunderlagResponse dosunderlagResponse = new DosunderlagResponse();
+	dosunderlagResponse.setDosproducent("Dosproducent");
+	dosunderlagResponse.setDosunderlagsversion(1);
+	ordinationslistaResponse.setDosunderlag(dosunderlagResponse);
+
+	return ordinationslistaResponse;
+    }
+
+}
