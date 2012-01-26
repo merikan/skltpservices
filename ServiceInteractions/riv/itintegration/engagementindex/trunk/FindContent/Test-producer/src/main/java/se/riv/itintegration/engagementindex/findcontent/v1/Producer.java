5e5a454bf27ae8ceb86e447fd77b672f43d26d8d
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-01-26 12:24:03 +0000

    Added test producer

---
+++ ServiceInteractions/riv/itintegration/engagementindex/trunk/FindContent/Test-producer/src/main/java/se/riv/itintegration/engagementindex/findcontent/v1/Producer.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2009 Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+
+ *   Boston, MA 02111-1307  USA
+ */
+package se.riv.itintegration.engagementindex.findcontent.v1;
+
+import javax.xml.ws.Endpoint;
+
+public class Producer {
+
+	protected Producer() throws Exception {
+		System.out.println("Starting Producer");
+		final Object implementor = new ProducerImpl();
+		// "https://localhost:23000/vp/FindContent/1/rivtabp21";
+		final String address = "http://localhost:23000/vp/FindContent/1/rivtabp21";
+		Endpoint.publish(address, implementor);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Producer();
+		System.out.println("Producer ready...");
+		Thread.sleep(5 * 60 * 1000);
+		System.out.println("Producer exiting");
+		System.exit(0);
+	}
+}
---
+++ ServiceInteractions/riv/itintegration/engagementindex/trunk/FindContent/Test-producer/src/main/java/se/riv/itintegration/engagementindex/findcontent/v1/ProducerImpl.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2009 Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+
+ *   Boston, MA 02111-1307  USA
+ */
+package se.riv.itintegration.engagementindex.findcontent.v1;
+
+import java.util.Date;
+
+import javax.jws.WebService;
+
+import riv.itintegration.engagementindex._1.EngagementType;
+import se.riv.itintegration.engagementindex.findcontent.v1.rivtabp21.FindContentResponderInterface;
+import se.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentResponseType;
+import se.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentType;
+
+@WebService(serviceName = "FindContentResponderService", endpointInterface = "se.riv.itintegration.engagementindex.findcontent.v1.rivtabp21.FindContentResponderInterface", portName = "FindContentResponderPort", targetNamespace = "urn:riv:itintegration:engagementindex:FindContent:1:rivtabp21", wsdlLocation = "schemas/interactions/FindContentInteraction/FindContentInteraction_1.0_RIVTABP21.wsdl")
+public class ProducerImpl implements FindContentResponderInterface {
+
+	@Override
+	public FindContentResponseType findContent(String arg0, FindContentType findContent) {
+
+		FindContentResponseType reponse = new FindContentResponseType();
+		reponse.getEngagement().add(createEngagementType(findContent));
+
+		return reponse;
+	}
+
+	private EngagementType createEngagementType(FindContentType findContent) {
+
+		EngagementType engagementType = new EngagementType();
+		engagementType.setBusinessObjectInstanceIdentifier(findContent.getBusinessObjectInstanceIdentifier());
+		engagementType.setCategorization(findContent.getCategorization());
+		engagementType.setCreationTime(new Date().toString());
+		engagementType.setHealthIssueThreadId(findContent.getHealthIssueThreadId());
+		engagementType.setLogicalAddress(findContent.getLogicalAddress());
+		engagementType.setMostRecentContent(findContent.getMostRecentContent());
+		engagementType.setOwner(findContent.getOwner());
+		engagementType.setRegisteredResidentIdentification(findContent.getRegisteredResidentIdentification());
+		engagementType.setServiceDomain(findContent.getServiceDomain());
+		engagementType.setSourceSystem(findContent.getSourceSystem());
+		engagementType.setUpdateTime(new Date().toString());
+
+		return engagementType;
+	}
+
+}
