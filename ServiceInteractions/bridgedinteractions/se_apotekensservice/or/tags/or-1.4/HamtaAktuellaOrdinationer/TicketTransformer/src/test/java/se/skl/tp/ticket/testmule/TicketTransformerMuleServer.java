656b61e8247d094059c08643972c53602342e638
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-03-29 17:17:01 +0000

    [maven-release-plugin]  copy for tag or-1.4

---
+++ ServiceInteractions/bridgedinteractions/se_apotekensservice/or/tags/or-1.4/HamtaAktuellaOrdinationer/TicketTransformer/src/test/java/se/skl/tp/ticket/testmule/TicketTransformerMuleServer.java
@@ -0,0 +1,31 @@
+package se.skl.tp.ticket.testmule;
+
+ 
+import org.soitoolkit.commons.mule.test.StandaloneMuleServer;
+ 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class TicketTransformerMuleServer {
+
+	private static final Logger logger = LoggerFactory.getLogger(TicketTransformerMuleServer.class);
+
+	public static final String MULE_SERVER_ID   = "tickettransformer";
+ 
+	public static final String MULE_CONFIG      = "TicketTransformer-teststubs-and-services-config.xml"; // both teststubs and services
+//	public static final String MULE_CONFIG      = "TicketTransformer-teststubs-only-config.xml"; // only teststubs
+//	public static final String MULE_CONFIG      = "vp-config.xml"; // only services
+
+	public static void main(String[] args) throws Exception {
+		
+	    	logger.info("Starting mule with args",args);
+	    
+		StandaloneMuleServer muleServer = new StandaloneMuleServer(MULE_SERVER_ID, MULE_CONFIG);
+ 
+		muleServer.run();
+	}
+	
+ 
+}
