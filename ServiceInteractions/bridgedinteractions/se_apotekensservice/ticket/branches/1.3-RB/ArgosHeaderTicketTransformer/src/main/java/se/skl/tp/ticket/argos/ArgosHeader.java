b553fdbaa61f37a2fa73f9aab85fd33e6c2a04b9
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-04-08 21:51:05 +0000

    just code format

--- ServiceInteractions/bridgedinteractions/se_apotekensservice/ticket/branches/1.3-RB/ArgosHeaderTicketTransformer/src/main/java/se/skl/tp/ticket/argos/ArgosHeader.java
+++ ServiceInteractions/bridgedinteractions/se_apotekensservice/ticket/branches/1.3-RB/ArgosHeaderTicketTransformer/src/main/java/se/skl/tp/ticket/argos/ArgosHeader.java
@@ -27,231 +27,231 @@ import org.apache.commons.lang.builder.ToStringBuilder;
  */
 public class ArgosHeader {
 
-    private String forskrivarkod;
-    private String legitimationskod;
-    private String fornamn;
-    private String efternamn;
-    private String yrkesgrupp;
-    private String befattningskod;
-    private String arbetsplatskod;
-    private String arbetsplatsnamn;
-    private String postort;
-    private String postadress;
-    private String postnummer;
-    private String telefonnummer;
-    private String requestId;
-    private String rollnamn;
-    private String directoryID;
-    private String hsaID;
-    private String katalog;
-    private String organisationsnummer;
-    private String systemnamn;
-    private String systemversion;
-    private String systemIp;
+	private String forskrivarkod;
+	private String legitimationskod;
+	private String fornamn;
+	private String efternamn;
+	private String yrkesgrupp;
+	private String befattningskod;
+	private String arbetsplatskod;
+	private String arbetsplatsnamn;
+	private String postort;
+	private String postadress;
+	private String postnummer;
+	private String telefonnummer;
+	private String requestId;
+	private String rollnamn;
+	private String directoryID;
+	private String hsaID;
+	private String katalog;
+	private String organisationsnummer;
+	private String systemnamn;
+	private String systemversion;
+	private String systemIp;
 
-    public ArgosHeader() {
-    }
+	public ArgosHeader() {
+	}
 
-    public ArgosHeader(String forskrivarkod, String legitimationskod, String fornamn, String efternamn,
-	    String yrkesgrupp, String befattningskod, String arbetsplatskod, String arbetsplatsnamn, String postort,
-	    String postadress, String postnummer, String telefonnummer, String requestId, String rollnamn,
-	    String directoryID, String hsaID, String katalog, String organisationsnummer, String systemnamn,
-	    String systemversion, String systemIp) {
+	public ArgosHeader(String forskrivarkod, String legitimationskod, String fornamn, String efternamn,
+			String yrkesgrupp, String befattningskod, String arbetsplatskod, String arbetsplatsnamn, String postort,
+			String postadress, String postnummer, String telefonnummer, String requestId, String rollnamn,
+			String directoryID, String hsaID, String katalog, String organisationsnummer, String systemnamn,
+			String systemversion, String systemIp) {
 
-	this.forskrivarkod = forskrivarkod;
-	this.legitimationskod = legitimationskod;
-	this.fornamn = fornamn;
-	this.efternamn = efternamn;
-	this.yrkesgrupp = yrkesgrupp;
-	this.befattningskod = befattningskod;
-	this.arbetsplatskod = arbetsplatskod;
-	this.arbetsplatsnamn = arbetsplatsnamn;
-	this.postort = postort;
-	this.postadress = postadress;
-	this.postnummer = postnummer;
-	this.telefonnummer = telefonnummer;
-	this.requestId = requestId;
-	this.rollnamn = rollnamn;
-	this.directoryID = directoryID;
-	this.hsaID = hsaID;
-	this.katalog = katalog;
-	this.organisationsnummer = organisationsnummer;
-	this.systemnamn = systemnamn;
-	this.systemversion = systemversion;
-	this.systemIp = systemIp;
-    }
+		this.forskrivarkod = forskrivarkod;
+		this.legitimationskod = legitimationskod;
+		this.fornamn = fornamn;
+		this.efternamn = efternamn;
+		this.yrkesgrupp = yrkesgrupp;
+		this.befattningskod = befattningskod;
+		this.arbetsplatskod = arbetsplatskod;
+		this.arbetsplatsnamn = arbetsplatsnamn;
+		this.postort = postort;
+		this.postadress = postadress;
+		this.postnummer = postnummer;
+		this.telefonnummer = telefonnummer;
+		this.requestId = requestId;
+		this.rollnamn = rollnamn;
+		this.directoryID = directoryID;
+		this.hsaID = hsaID;
+		this.katalog = katalog;
+		this.organisationsnummer = organisationsnummer;
+		this.systemnamn = systemnamn;
+		this.systemversion = systemversion;
+		this.systemIp = systemIp;
+	}
 
-    public void setForskrivarkod(String forskrivarkod) {
-	this.forskrivarkod = forskrivarkod;
-    }
+	public void setForskrivarkod(String forskrivarkod) {
+		this.forskrivarkod = forskrivarkod;
+	}
 
-    public void setLegitimationskod(String legitimationskod) {
-	this.legitimationskod = legitimationskod;
-    }
+	public void setLegitimationskod(String legitimationskod) {
+		this.legitimationskod = legitimationskod;
+	}
 
-    public void setFornamn(String fornamn) {
-	this.fornamn = fornamn;
-    }
+	public void setFornamn(String fornamn) {
+		this.fornamn = fornamn;
+	}
 
-    public void setEfternamn(String efternamn) {
-	this.efternamn = efternamn;
-    }
+	public void setEfternamn(String efternamn) {
+		this.efternamn = efternamn;
+	}
 
-    public void setYrkesGrupp(String yrkesgrupp) {
-	this.yrkesgrupp = yrkesgrupp;
-    }
+	public void setYrkesGrupp(String yrkesgrupp) {
+		this.yrkesgrupp = yrkesgrupp;
+	}
 
-    public void setBefattningskod(String befattningskod) {
-	this.befattningskod = befattningskod;
-    }
+	public void setBefattningskod(String befattningskod) {
+		this.befattningskod = befattningskod;
+	}
 
-    public void setArbetsplatskod(String arbetsplatskod) {
-	this.arbetsplatskod = arbetsplatskod;
-    }
+	public void setArbetsplatskod(String arbetsplatskod) {
+		this.arbetsplatskod = arbetsplatskod;
+	}
 
-    public void setArbetsplatsnamn(String arbetsplatsnamn) {
-	this.arbetsplatsnamn = arbetsplatsnamn;
-    }
+	public void setArbetsplatsnamn(String arbetsplatsnamn) {
+		this.arbetsplatsnamn = arbetsplatsnamn;
+	}
 
-    public void setPostort(String postort) {
-	this.postort = postort;
-    }
+	public void setPostort(String postort) {
+		this.postort = postort;
+	}
 
-    public void setPostadress(String postadress) {
-	this.postadress = postadress;
-    }
+	public void setPostadress(String postadress) {
+		this.postadress = postadress;
+	}
 
-    public void setPostnummer(String postnummer) {
-	this.postnummer = postnummer;
-    }
+	public void setPostnummer(String postnummer) {
+		this.postnummer = postnummer;
+	}
 
-    public void setTelefonnummer(String telefonnummer) {
-	this.telefonnummer = telefonnummer;
-    }
+	public void setTelefonnummer(String telefonnummer) {
+		this.telefonnummer = telefonnummer;
+	}
 
-    public void setRequestId(String requestId) {
-	this.requestId = requestId;
-    }
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
 
-    public void setRollnamn(String rollnamn) {
-	this.rollnamn = rollnamn;
-    }
+	public void setRollnamn(String rollnamn) {
+		this.rollnamn = rollnamn;
+	}
 
-    public void setDirectoryID(String directoryID) {
-	this.directoryID = directoryID;
-    }
+	public void setDirectoryID(String directoryID) {
+		this.directoryID = directoryID;
+	}
 
-    public void setHsaID(String hsaID) {
-	this.hsaID = hsaID;
-    }
+	public void setHsaID(String hsaID) {
+		this.hsaID = hsaID;
+	}
 
-    public void setKatalog(String katalog) {
-	this.katalog = katalog;
-    }
+	public void setKatalog(String katalog) {
+		this.katalog = katalog;
+	}
 
-    public void setOrganisationsnummer(String organisationsnummer) {
-	this.organisationsnummer = organisationsnummer;
-    }
+	public void setOrganisationsnummer(String organisationsnummer) {
+		this.organisationsnummer = organisationsnummer;
+	}
 
-    public void setSystemnamn(String systemnamn) {
-	this.systemnamn = systemnamn;
-    }
+	public void setSystemnamn(String systemnamn) {
+		this.systemnamn = systemnamn;
+	}
 
-    public void setSystemversion(String systemversion) {
-	this.systemversion = systemversion;
-    }
+	public void setSystemversion(String systemversion) {
+		this.systemversion = systemversion;
+	}
 
-    public void setSystemIp(String systemIp) {
-	this.systemIp = systemIp;
-    }
+	public void setSystemIp(String systemIp) {
+		this.systemIp = systemIp;
+	}
 
-    public String getForskrivarkod() {
-	return forskrivarkod;
-    }
+	public String getForskrivarkod() {
+		return forskrivarkod;
+	}
 
-    public String getLegitimationskod() {
-	return legitimationskod;
-    }
+	public String getLegitimationskod() {
+		return legitimationskod;
+	}
 
-    public String getFornamn() {
-	return fornamn;
-    }
+	public String getFornamn() {
+		return fornamn;
+	}
 
-    public String getEfternamn() {
-	return efternamn;
-    }
+	public String getEfternamn() {
+		return efternamn;
+	}
 
-    public String getYrkesgrupp() {
-	return yrkesgrupp;
-    }
+	public String getYrkesgrupp() {
+		return yrkesgrupp;
+	}
 
-    public String getBefattningskod() {
-	return befattningskod;
-    }
+	public String getBefattningskod() {
+		return befattningskod;
+	}
 
-    public String getArbetsplatskod() {
-	return arbetsplatskod;
-    }
+	public String getArbetsplatskod() {
+		return arbetsplatskod;
+	}
 
-    public String getArbetsplatsnamn() {
-	return arbetsplatsnamn;
-    }
+	public String getArbetsplatsnamn() {
+		return arbetsplatsnamn;
+	}
 
-    public String getPostort() {
-	return postort;
-    }
+	public String getPostort() {
+		return postort;
+	}
 
-    public String getPostadress() {
-	return postadress;
-    }
+	public String getPostadress() {
+		return postadress;
+	}
 
-    public String getPostnummer() {
-	return postnummer;
-    }
+	public String getPostnummer() {
+		return postnummer;
+	}
 
-    public String getTelefonnummer() {
-	return telefonnummer;
-    }
+	public String getTelefonnummer() {
+		return telefonnummer;
+	}
 
-    public String getRequestId() {
-	return requestId;
-    }
+	public String getRequestId() {
+		return requestId;
+	}
 
-    public String getRollnamn() {
-	return rollnamn;
-    }
+	public String getRollnamn() {
+		return rollnamn;
+	}
 
-    public String getDirectoryID() {
-	return directoryID;
-    }
+	public String getDirectoryID() {
+		return directoryID;
+	}
 
-    public String getHsaID() {
-	return hsaID;
-    }
+	public String getHsaID() {
+		return hsaID;
+	}
 
-    public String getKatalog() {
-	return katalog;
-    }
+	public String getKatalog() {
+		return katalog;
+	}
 
-    public String getOrganisationsnummer() {
-	return organisationsnummer;
-    }
+	public String getOrganisationsnummer() {
+		return organisationsnummer;
+	}
 
-    public String getSystemnamn() {
-	return systemnamn;
-    }
+	public String getSystemnamn() {
+		return systemnamn;
+	}
 
-    public String getSystemversion() {
-	return systemversion;
-    }
+	public String getSystemversion() {
+		return systemversion;
+	}
 
-    public String getSystemIp() {
-	return systemIp;
-    }
+	public String getSystemIp() {
+		return systemIp;
+	}
 
-    @Override
-    public String toString() {
-	return ToStringBuilder.reflectionToString(this);
-    }
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 
 }
