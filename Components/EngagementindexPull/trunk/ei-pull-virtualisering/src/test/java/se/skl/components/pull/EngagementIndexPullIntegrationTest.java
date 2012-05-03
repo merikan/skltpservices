5da5870877c87a292e1eac64ce6ace6ccf5a660e
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-05-03 08:45:47 +0000

    Config file and test

---
+++ Components/EngagementindexPull/trunk/ei-pull-virtualisering/src/test/java/se/skl/components/pull/EngagementIndexPullIntegrationTest.java
@@ -0,0 +1,33 @@
+package se.skl.components.pull;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.soitoolkit.commons.mule.test.AbstractTestCase;
+
+public class EngagementIndexPullIntegrationTest extends AbstractTestCase {
+
+	@BeforeClass
+	public void beforeClass() {
+		setDisposeManagerPerSuite(true);
+		setTestTimeoutSecs(240);
+	}
+
+	@Before
+	public void doSetUp() throws Exception {
+		super.doSetUp();
+		setDisposeManagerPerSuite(true);
+	}
+
+	@Override
+	protected String getConfigResources() {
+		return "teststubs-and-services-config.xml";
+	}
+
+	@Test
+	public void testSomething() {
+		Assert.assertTrue(true);
+	}
+
+}
