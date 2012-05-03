3b60a591d48c76f0e89d9ba432fe3ff3b8a9317a
hans.thunberg <hans.thunberg@callistaenterprise.se>
2012-05-03 05:29:48 +0000

    Testproducer, initial pull component

--- Components/EngagementindexPull/trunk/ei-pull-virtualisering/src/test/java/se/riv/itintegration/engagementindex/GetUpdatesTestProducer.java
+++ Components/EngagementindexPull/trunk/ei-pull-virtualisering/src/test/java/se/riv/itintegration/engagementindex/GetUpdatesTestProducer.java
@@ -1,22 +1,56 @@
 package se.riv.itintegration.engagementindex;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 import javax.jws.WebService;
 
+import riv.itintegration.engagementindex._1.EngagementType;
+import riv.itintegration.engagementindex._1.RegisteredResidentEngagementType;
 import se.riv.itintegration.engagementindex.getupdates.v1.rivtabp21.GetUpdatesResponderInterface;
 import se.riv.itintegration.engagementindex.getupdatesresponder.v1.GetUpdatesResponseType;
 import se.riv.itintegration.engagementindex.getupdatesresponder.v1.GetUpdatesType;
 
-@WebService(serviceName = "GetUpdatesResponderService", 
-			endpointInterface = "se.riv.itintegration.engagementindex.getupdates.v1.rivtabp21.GetUpdatesResponderInterface",
-			portName = "GetUpdatesResponderPort", 
-			targetNamespace = "urn:riv:itintegration:engagementindex:GetUpdates:1:rivtabp21", 
-			wsdlLocation = "schemas/interactions/GetUpdatesInteraction/GetUpdatesInteraction_1.0_RIVTABP21.wsdl")
-public class GetUpdatesTestProducer implements GetUpdatesResponderInterface{
+@WebService(serviceName = "GetUpdatesResponderService", endpointInterface = "se.riv.itintegration.engagementindex.getupdates.v1.rivtabp21.GetUpdatesResponderInterface", portName = "GetUpdatesResponderPort", targetNamespace = "urn:riv:itintegration:engagementindex:GetUpdates:1:rivtabp21", wsdlLocation = "schemas/interactions/GetUpdatesInteraction/GetUpdatesInteraction_1.0_RIVTABP21.wsdl")
+public class GetUpdatesTestProducer implements GetUpdatesResponderInterface {
+
+	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
 
 	@Override
-	public GetUpdatesResponseType getUpdates(String arg0, GetUpdatesType arg1) {
+	public GetUpdatesResponseType getUpdates(String arg0, GetUpdatesType request) {
+
+		String serviceDomain = request.getServiceDomain();
+
 		GetUpdatesResponseType response = new GetUpdatesResponseType();
+		response.setResponseIsComplete(true);
+		response.getRegisteredResidentEngagement().add(
+				createRegisteredResidentEngagementType(serviceDomain, "197303160555"));
 		return response;
+	}
+
+	private RegisteredResidentEngagementType createRegisteredResidentEngagementType(String serviceDomain,
+			String registeredResidentIdentification) {
+		RegisteredResidentEngagementType response = new RegisteredResidentEngagementType();
+		response.setRegisteredResidentIdentification(registeredResidentIdentification);
+		response.getEngagement().add(createEngagement(serviceDomain, registeredResidentIdentification));
+		return response;
+	}
+
+	private EngagementType createEngagement(String serviceDomain, String registeredResidentIdentification) {
+		EngagementType engagement = new EngagementType();
+		engagement.setBusinessObjectInstanceIdentifier("bookingId");
+		engagement.setCategorization("Booking");
+		engagement.setClinicalProcessInterestId(UUID.randomUUID().toString());
+		engagement.setCreationTime(dateFormat.format(new Date()));
+		engagement.setLogicalAddress("Landstingets hsaid:Vårdgivarens HSA-id:Enhetens hsaid");
+		engagement.setMostRecentContent(dateFormat.format(new Date()));
+		engagement.setOwner("HSA-id");
+		engagement.setRegisteredResidentIdentification(registeredResidentIdentification);
+		engagement.setServiceDomain(serviceDomain);
+		engagement.setSourceSystem("Systemets HSA-ID");
+		engagement.setUpdateTime(dateFormat.format(new Date()));
+		return engagement;
 	}
 
 }
--- Components/EngagementindexPull/trunk/ei-pull/src/main/java/se/skl/components/pull/EngagementIndexPull.java
+++ Components/EngagementindexPull/trunk/ei-pull/src/main/java/se/skl/components/pull/EngagementIndexPull.java
@@ -1,9 +1,78 @@
 package se.skl.components.pull;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import riv.itintegration.engagementindex._1.EngagementTransactionType;
+import riv.itintegration.engagementindex._1.EngagementType;
+import riv.itintegration.engagementindex._1.RegisteredResidentEngagementType;
+import se.riv.itintegration.engagementindex.getupdates.v1.rivtabp21.GetUpdatesResponderInterface;
+import se.riv.itintegration.engagementindex.getupdatesresponder.v1.GetUpdatesResponseType;
+import se.riv.itintegration.engagementindex.getupdatesresponder.v1.GetUpdatesType;
+import se.riv.itintegration.engagementindex.update.v1.rivtabp21.UpdateResponderInterface;
+import se.riv.itintegration.engagementindex.updateresponder.v1.UpdateType;
+
+@Component
 public class EngagementIndexPull {
 
-	public String pull() {
-		return "Pull started";
+	@Autowired
+	GetUpdatesResponderInterface getUpdatesClient;
+
+	@Autowired
+	UpdateResponderInterface updateClient;
+
+	public void doPull() {
+		GetUpdatesResponseType updates = pull("Kalle");
+		push("Kalle", updates);
+	}
+
+	private GetUpdatesResponseType pull(String logicalAddress) {
+		GetUpdatesType updateRequest = new GetUpdatesType();
+		updateRequest.setServiceDomain("riv:crm:scheduling");
+		updateRequest.setTimeStamp(getTimestamp());
+
+		GetUpdatesResponseType response = getUpdatesClient.getUpdates(logicalAddress, updateRequest);
+		return response;
+	}
+
+	private void push(String logicalAddress, GetUpdatesResponseType updates) {
+		UpdateType requestForUpdate = createRequestForUpdate(updates);
+		updateClient.update(logicalAddress, requestForUpdate);
+	}
+
+	private UpdateType createRequestForUpdate(GetUpdatesResponseType updateResponse) {
+		UpdateType requestForUpdate = new UpdateType();
+		List<RegisteredResidentEngagementType> updates = updateResponse.getRegisteredResidentEngagement();
+		for (RegisteredResidentEngagementType registeredResidentEngagementType : updates) {
+			List<EngagementType> engagements = registeredResidentEngagementType.getEngagement();
+			for (EngagementType engagementType : engagements) {
+				EngagementTransactionType engagementTransaction = new EngagementTransactionType();
+				// Var hittar man informtion om vad som skall tas bort?
+				// engagementTransaction.setDeleteFlag(value);
+				engagementTransaction.setEngagement(engagementType);
+				requestForUpdate.getEngagementTransaction().add(engagementTransaction);
+			}
+		}
+
+		return requestForUpdate;
+	}
+
+	String getTimestamp() {
+		return new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationcontext.xml");
+		BeanFactory factory = context;
+		EngagementIndexPull engagementIndexPull = (EngagementIndexPull) factory.getBean("engagementIndexPull");
+		//engagementIndexPull.doPull("LOGICALADDRESS");
 	}
 
 }
