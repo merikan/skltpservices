a6060febeea876da1bd3b91e668a3684049e1faf
henrik.rostam <henrik.rostam@callistaenterprise.se>
2012-05-06 17:46:04 +0000

    Adding fetching of addresses

--- Components/EngagementindexPull/trunk/ei-pull-virtualisering/src/test/java/se/riv/itintegration/registry/GetLogicalAddresseesByServiceContractTestProducer.java
+++ Components/EngagementindexPull/trunk/ei-pull-virtualisering/src/test/java/se/riv/itintegration/registry/GetLogicalAddresseesByServiceContractTestProducer.java
@@ -2,24 +2,34 @@ package se.riv.itintegration.registry;
 
 import javax.jws.WebService;
 
+import org.mule.util.StringUtils;
 import se.riv.itintegration.registry.getlogicaladdresseesbyservicecontract.v1.rivtabp21.GetLogicalAddresseesByServiceContractResponderInterface;
 import se.riv.itintegration.registry.getlogicaladdresseesbyservicecontractresponder.v1.GetLogicalAddresseesByServiceContractResponseType;
 import se.riv.itintegration.registry.getlogicaladdresseesbyservicecontractresponder.v1.GetLogicalAddresseesByServiceContractType;
+import se.riv.itintegration.registry.v1.ServiceContractNamespaceType;
+
+import java.util.ArrayList;
 
 @WebService(serviceName = "GetLogicalAddresseesByServiceContractResponderService", 
 			endpointInterface = "se.riv.itintegration.registry.getlogicaladdresseesbyservicecontract.v1.rivtabp21.GetLogicalAddresseesByServiceContractResponderInterface",
 			portName = "GetLogicalAddresseesByServiceContractResponderPort", 
 			targetNamespace = "urn:riv:itintegration:registry:GetLogicalAddresseesByServiceContract:1:rivtabp21", 
 			wsdlLocation = "schemas/interactions/GetLogicalAddresseesByServiceContractInteraction/GetLogicalAddresseesByServiceContractInteraction_1.0_RIVTABP21.wsdl")
-public class GetLogicalAddresseesByServiceContractTestProducer implements
-		GetLogicalAddresseesByServiceContractResponderInterface {
+public class GetLogicalAddresseesByServiceContractTestProducer implements GetLogicalAddresseesByServiceContractResponderInterface {
 
 	@Override
-	public GetLogicalAddresseesByServiceContractResponseType getLogicalAddresseesByServiceContract(String arg0,
-			GetLogicalAddresseesByServiceContractType arg1) {
-		
+	public GetLogicalAddresseesByServiceContractResponseType getLogicalAddresseesByServiceContract(String logicalAddress, GetLogicalAddresseesByServiceContractType parameters) {
+        String serviceConsumerHsaId = parameters.getServiceConsumerHsaId();
+        ServiceContractNamespaceType serviceContractNamespaceType = parameters.getServiceContractNameSpace();
 		GetLogicalAddresseesByServiceContractResponseType response = new GetLogicalAddresseesByServiceContractResponseType();
-		
+        ArrayList<String> logicalAddresses = new ArrayList<String>();
+        if (StringUtils.equals(logicalAddress, "Kalle")) {
+            logicalAddresses.add("Lion");
+            logicalAddresses.add("Cliff Valley");
+            logicalAddresses.add("Aoaoao");
+        }
+        // Not nice pattern but JAX only generates a getter.
+        response.getLogicalAddress().addAll(logicalAddresses);
 		return response;
 	}
 
