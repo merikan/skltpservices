09aaa49447de871ba1eba4095b310cb4a5883927
mats.ekhammar@callistaenterprise.se <mats.ekhammar@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-05-23 09:53:35 +0000

    release of 1.1

---
+++ ServiceInteractions/riv/crm/scheduling/tags/crm-scheduling-1.1/GetSubjectOfCareSchedule/Test-consumer/src/main/java/se/riv/crm/scheduling/getsubjectofcareschedule/v1/TestConsumer.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright 2009 Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+
+ *   Boston, MA 02111-1307  USA
+ */
+package se.riv.crm.scheduling.getsubjectofcareschedule.v1;
+
+import java.io.FileInputStream;
+import java.net.URL;
+import java.security.KeyStore;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManagerFactory;
+import javax.xml.ws.BindingProvider;
+
+import org.apache.cxf.configuration.jsse.TLSClientParameters;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
+
+import se.riv.crm.scheduling.getsubjectofcareschedule.v1.rivtabp21.GetSubjectOfCareScheduleResponderInterface;
+import se.riv.crm.scheduling.getsubjectofcareschedule.v1.rivtabp21.GetSubjectOfCareScheduleResponderService;
+import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.GetSubjectOfCareScheduleResponseType;
+import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.GetSubjectOfCareScheduleType;
+
+public final class TestConsumer {
+
+	private static final String LOGICAL_ADDRESS = "Test";
+
+	private static final String LOGISK_ADDRESS = "/GetSubjectOfCareSchedule/1/rivtabp21";
+
+	// private static String host = "192.168.25.40:20000/vp";
+
+	private static String host = "test1.esb.ntjp.se/vp";
+
+	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			host = args[0];
+		}
+
+		String endpointAddress = "https://" + host + LOGISK_ADDRESS;
+		System.out.println("Consumer connecting to " + endpointAddress);
+		String p = callService2(endpointAddress, "Test");
+		System.out.println("Returned: " + p);
+	}
+
+	public static String callService2(String serviceAddress, String logicalAddresss) {
+
+		try {
+			// Get URL to wsdl file
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			URL wsdlUrl = loader
+					.getResource("schemas/interactions/GetSubjectOfCareScheduleInteraction/GetSubjectOfCareScheduleInteraction_1.1_RIVTABP21.wsdl");
+
+			GetSubjectOfCareScheduleResponderService service = new GetSubjectOfCareScheduleResponderService(wsdlUrl);
+			GetSubjectOfCareScheduleResponderInterface serviceInterface = service
+					.getGetSubjectOfCareScheduleResponderPort();
+
+			// Set web service server url
+			BindingProvider provider = (BindingProvider) serviceInterface;
+			provider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, serviceAddress);
+
+			GetSubjectOfCareScheduleType request = new GetSubjectOfCareScheduleType();
+			request.setHealthcareFacility("HSA-VKK123");
+			request.setSubjectOfCare("19751026-6849");
+
+			Client client = ClientProxy.getClient(serviceInterface);
+			HTTPConduit http = (HTTPConduit) client.getConduit();
+
+			HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
+			httpClientPolicy.setConnectionTimeout(36000);
+			httpClientPolicy.setAllowChunking(false);
+			httpClientPolicy.setReceiveTimeout(32000);
+			http.setClient(httpClientPolicy);
+
+			TLSClientParameters tlsCP = setUpTlsClientParams();
+			http.setTlsClientParameters(tlsCP);
+
+			GetSubjectOfCareScheduleResponseType response = serviceInterface.getSubjectOfCareSchedule(LOGICAL_ADDRESS,
+					null, request);
+
+			return ("GetSubjectOfCareSchedule response=" + response.getTimeslotDetail());
+
+		} catch (Throwable ex) {
+			System.out.println("Exception={}" + ex.getMessage());
+			throw new RuntimeException(ex); // TODO: Define other exception
+		}
+	}
+
+	private static TLSClientParameters setUpTlsClientParams() throws Exception {
+
+		KeyStore trustStore = KeyStore.getInstance("JKS");
+		String trustStoreLoc = "../../certs/truststore_test.jks";
+		String trustPassword = "password";
+		trustStore.load(new FileInputStream(trustStoreLoc), trustPassword.toCharArray());
+
+		String keyPassword = "password";
+		KeyStore keyStore = KeyStore.getInstance("PKCS12");
+		String keyStoreLoc = "../../certs/consumer.p12";
+		keyStore.load(new FileInputStream(keyStoreLoc), keyPassword.toCharArray());
+
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		tmf.init(trustStore);
+
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		kmf.init(keyStore, keyPassword.toCharArray());
+
+		TLSClientParameters tlsCP = new TLSClientParameters();
+		tlsCP.setTrustManagers(tmf.getTrustManagers());
+		tlsCP.setKeyManagers(kmf.getKeyManagers());
+
+		// The following is not recommended and would not be done in a
+		// prodcution environment,
+		// this is just for illustrative purpose
+		tlsCP.setDisableCNCheck(true);
+
+		return tlsCP;
+
+	}
+
+}
---
+++ ServiceInteractions/riv/crm/scheduling/tags/crm-scheduling-1.1/GetSubjectOfCareSchedule/Test-producer/src/main/java/se/riv/crm/scheduling/getsubjectofcareschedule/v1/ProducerImpl.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2009 Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+
+ *   Boston, MA 02111-1307  USA
+ */
+package se.riv.crm.scheduling.getsubjectofcareschedule.v1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.jws.WebService;
+
+import riv.interoperability.headers._1.ActorType;
+import se.riv.crm.scheduling.getsubjectofcareschedule.v1.rivtabp21.GetSubjectOfCareScheduleResponderInterface;
+import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.GetSubjectOfCareScheduleResponseType;
+import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.GetSubjectOfCareScheduleType;
+import se.riv.crm.scheduling.v1.TimeslotType;
+
+
+@WebService(
+		serviceName = "GetSubjectOfCareScheduleResponderService", 
+		endpointInterface="se.riv.crm.scheduling.getsubjectofcareschedule.v1.rivtabp21.GetSubjectOfCareScheduleResponderInterface", 
+		portName = "GetSubjectOfCareScheduleResponderPort", 
+		targetNamespace = "urn:riv:crm:scheduling:GetSubjectOfCareSchedule:1:rivtabp21",
+		wsdlLocation = "schemas/interactions/GetSubjectOfCareScheduleInteraction/GetSubjectOfCareScheduleInteraction_1.1_RIVTABP21.wsdl")
+public class ProducerImpl implements GetSubjectOfCareScheduleResponderInterface {
+	
+	static final SimpleDateFormat timeFformat = new SimpleDateFormat("yyyyMMddHHmmss");
+
+	@Override
+	public GetSubjectOfCareScheduleResponseType getSubjectOfCareSchedule(String arg0, ActorType arg1,
+			GetSubjectOfCareScheduleType arg2) {
+		GetSubjectOfCareScheduleResponseType careScheduleResponseType = new GetSubjectOfCareScheduleResponseType();
+		careScheduleResponseType.getTimeslotDetail().add(crateTimeslotType());
+		careScheduleResponseType.getTimeslotDetail().add(crateTimeslotType());
+		return careScheduleResponseType;
+	}
+
+	private TimeslotType crateTimeslotType() {
+		TimeslotType timeslotType = new TimeslotType();
+		timeslotType.setBookingId(UUID.randomUUID().toString());
+		timeslotType.setCareTypeID("CareTypeID");
+		timeslotType.setCareTypeName("CareTypeName");
+		timeslotType.setEndTimeExclusive(timeFformat.format(new Date()));
+		timeslotType.setHealthcareFacility("HaltcareFacility");
+		timeslotType.setHealthcareFacilityName("HaltcareFacilityName");
+		timeslotType.setPerformer("Performer");
+		timeslotType.setPerformerName("PerformerName");
+		timeslotType.setPurpose("Purpose");
+		timeslotType.setReason("Reason");
+		timeslotType.setResourceID("ResourceID");
+		timeslotType.setResourceName("ResourceName");
+		timeslotType.setStartTimeInclusive(timeFformat.format(new Date()));
+		timeslotType.setSubjectOfCare("SubjectOfCare");
+		timeslotType.setTimeTypeID("TimeTypeID");
+		timeslotType.setTimeTypeName("TimeTypeName");
+		return timeslotType;
+	}
+
+	
+}
