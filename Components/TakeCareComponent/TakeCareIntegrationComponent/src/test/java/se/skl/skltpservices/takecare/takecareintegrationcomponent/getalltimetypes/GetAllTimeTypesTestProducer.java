d2552fb4641db322c40ff0a7c398752216efc991
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-06-07 06:54:51 +0000

    Updated to Tidbok 1.0 and changed packagenames to conform to skl standards

--- Components/TakeCareComponent/TakeCareIntegrationComponent/src/test/java/se/skl/skltpservices/takecare/takecareintegrationcomponent/getalltimetypes/GetAllTimeTypesTestProducer.java
+++ Components/TakeCareComponent/TakeCareIntegrationComponent/src/test/java/se/skl/skltpservices/takecare/takecareintegrationcomponent/getalltimetypes/GetAllTimeTypesTestProducer.java
@@ -1,55 +1,58 @@
-package se.skl.takecare.takecareintegrationcomponent.getalltimetypes;
-
-import java.util.StringTokenizer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.soitoolkit.commons.mule.util.RecursiveResourceBundle;
-
-
+package se.skl.skltpservices.takecare.takecareintegrationcomponent.getalltimetypes;
 
 import javax.jws.WebService;
 
-import org.soitoolkit.refapps.sd.sample.schema.v1.Sample;
-import org.soitoolkit.refapps.sd.sample.schema.v1.SampleResponse;
-import org.soitoolkit.refapps.sd.sample.wsdl.v1.Fault;
-import org.soitoolkit.refapps.sd.sample.wsdl.v1.SampleInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soitoolkit.commons.mule.util.RecursiveResourceBundle;
+import org.w3c.addressing_1_0.AttributedURIType;
 
-@WebService(serviceName = "sampleService", portName = "samplePort", targetNamespace = "urn:org.soitoolkit.refapps.sd.sample.wsdl:v1", name = "sampleService")
-public class GetAllTimeTypesTestProducer implements SampleInterface {
+import se.riv.crm.scheduling_1.GetAllTimeTypesResponderInterface;
+import se.riv.crm.scheduling_1.GetAllTimeTypesResponseType;
+import se.riv.crm.scheduling_1.GetAllTimeTypesType;
+import se.riv.crm.scheduling_1.TimeTypeType;
 
-	public static final String TEST_ID_OK               = "1234567890";
-	public static final String TEST_ID_FAULT_INVALID_ID = "-1";
-	public static final String TEST_ID_FAULT_TIMEOUT    = "0";
-	
+@WebService(serviceName = "GetAllTimeTypesResponderService", portName = "GetAllTimeTypesResponderPort", targetNamespace = "urn:riv:crm:scheduling:GetAllTimeTypes:1:rivtabp20", name = "getAllTimeTypesService")
+public class GetAllTimeTypesTestProducer implements GetAllTimeTypesResponderInterface {
+
+	public static final String TEST_HEALTHCAREFACILITY_OK = "HSA-VKK123";
+
+	public static final String TEST_HEALTHCAREFACILITY_INVALID_ID = "-1";
+
+	public static final String TEST_ID_FAULT_TIMEOUT = "0";
+
 	private static final Logger log = LoggerFactory.getLogger(GetAllTimeTypesTestProducer.class);
-    private static final RecursiveResourceBundle rb = new RecursiveResourceBundle("TakeCareIntegrationComponent-config");
+	private static final RecursiveResourceBundle rb = new RecursiveResourceBundle("TakeCareIntegrationComponent-config");
 	private static final long SERVICE_TIMOUT_MS = Long.parseLong(rb.getString("SERVICE_TIMEOUT_MS"));
 
-	public SampleResponse sample(Sample request) throws Fault {
+	private TimeTypeType createTimeType(String timeTypeId, String timeTypeName) {
+		TimeTypeType timeTypeType = new TimeTypeType();
+		timeTypeType.setTimeTypeId(timeTypeId);
+		timeTypeType.setTimeTypeName(timeTypeName);
+		return timeTypeType;
+	}
 
+	public GetAllTimeTypesResponseType getAllTimeTypes(AttributedURIType arg0, GetAllTimeTypesType request) {
 		log.info("GetAllTimeTypesTestProducer received the request: {}", request);
 
-		String id = request.getId();
+		String healthcareFacility = request.getHealthcareFacility();
 
 		// Return an error-message if invalid id
-		if (TEST_ID_FAULT_INVALID_ID.equals(id)) {
-			throw new Fault("Invalid Id: " + id);
+		if (TEST_HEALTHCAREFACILITY_INVALID_ID.equals(healthcareFacility)) {
+			throw new RuntimeException("Invalid Id: " + healthcareFacility);
 		}
 
 		// Force a timeout if zero Id
-        if (TEST_ID_FAULT_TIMEOUT.equals(id)) {
-	    	try {
+		if (TEST_ID_FAULT_TIMEOUT.equals(healthcareFacility)) {
+			try {
 				Thread.sleep(SERVICE_TIMOUT_MS + 1000);
-			} catch (InterruptedException e) {}
-        }
+			} catch (InterruptedException e) {
+			}
+		}
 
-        // Produce the response
-		SampleResponse response = new SampleResponse();
-		response.setValue("Value" + id);
-		return response;
+		// Produce the response
+		GetAllTimeTypesResponseType responseType = new GetAllTimeTypesResponseType();
+		responseType.getListOfTimeTypes().add(createTimeType("0", "OPEN"));
+		return responseType;
 	}
 }
-
-
