fa44e3a772cb7fe46ec8af8984bdace5a4892a60
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-06-14 19:40:51 +0000

    Moved classes that was placed in test by mistake

---
+++ Components/TakeCareComponent/TakeCareIntegrationComponent/src/main/java/se/skl/skltpservices/takecare/TakeCareResponseTransformer.java
@@ -0,0 +1,38 @@
+package se.skl.skltpservices.takecare;
+
+import org.apache.commons.lang.StringUtils;
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageTransformer;
+import org.soitoolkit.commons.mule.jaxb.JaxbUtil;
+
+import se.skl.skltpservices.takecare.booking.error.ProfdocHISMessage;
+
+public abstract class TakeCareResponseTransformer extends AbstractMessageTransformer {
+
+	private static final JaxbUtil jaxbUtil_error = new JaxbUtil(ProfdocHISMessage.class);
+
+	/**
+	 * Message aware transformer.
+	 */
+	@Override
+	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+		if (message.getExceptionPayload() != null) {
+			return message;
+		}
+		return pojoTransform(message.getPayload(), outputEncoding);
+	}
+
+	protected abstract Object pojoTransform(Object src, String outputEncoding) throws TransformerException;
+
+	protected void createErrorFromProfdocHISErrorMessage(String incoming_string) {
+		ProfdocHISMessage message = (ProfdocHISMessage) jaxbUtil_error.unmarshal(incoming_string);
+		throw new RuntimeException("resultCode: " + message.getError().getCode() + " resultText: "
+				+ message.getError().getMsg());
+	}
+
+	protected boolean containsError(String incoming_string) {
+		return StringUtils.contains(incoming_string, "Error");
+	}
+
+}
---
+++ Components/TakeCareComponent/TakeCareIntegrationComponent/src/main/java/se/skl/skltpservices/takecare/TakeCareTestProducer.java
@@ -0,0 +1,78 @@
+package se.skl.skltpservices.takecare;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+import org.apache.commons.lang.time.FastDateFormat;
+import org.soitoolkit.commons.mule.jaxb.JaxbUtil;
+
+import se.skl.skltpservices.takecare.booking.error.ProfdocHISMessage;
+
+public class TakeCareTestProducer {
+
+	private static final JaxbUtil jaxbUtil_error = new JaxbUtil(ProfdocHISMessage.class);
+
+	protected BigInteger yyyyMMddHHmmss(Date date) {
+		FastDateFormat dateFormat = FastDateFormat.getInstance("yyyyMMddHHmmss");
+		return new BigInteger(dateFormat.format(date));
+	}
+
+	protected BigInteger yyyyMMddHHmm(Date date) {
+		FastDateFormat dateFormat = FastDateFormat.getInstance("yyyyMMddHHmm");
+		return new BigInteger(dateFormat.format(date));
+	}
+
+	protected String createErrorResponse(String careunitId, String externalUser) {
+
+		se.skl.skltpservices.takecare.booking.error.ProfdocHISMessage error_response = new se.skl.skltpservices.takecare.booking.error.ProfdocHISMessage();
+		error_response.setCareUnitType("hsaid");
+		error_response.setCareUnit(careunitId);
+		error_response.setError(new se.skl.skltpservices.takecare.booking.error.ProfdocHISMessage.Error());
+		error_response.getError().setCode(3001);
+		error_response.getError().setMsg("Illegal argument!");
+		error_response.getError().setType("System");
+		error_response.setMethod("Booking.GetTimeTypes");
+		error_response.setMsgType("Error");
+		error_response.setSystem("ProfdocHIS");
+		error_response.setSystemInstance(0);
+		error_response.setTime(yyyyMMddHHmmss(new Date()));
+		error_response.setUser(externalUser);
+		return jaxbUtil_error.marshal(error_response);
+	}
+
+	public String getAvailableDates(String tcusername, String tcpassword, String externaluser, String careunitidtype,
+			String careunitid, String xml) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getAvailableTimeslots(String tcusername, String tcpassword, String externaluser,
+			String careunitidtype, String careunitid, String xml) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String cancelBooking(String tcusername, String tcpassword, String externaluser, String careunitidtype,
+			String careunitid, String xml) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getTimeTypes(String tcusername, String tcpassword, String externaluser, String careunitidtype,
+			String careunitid, String xml) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getBookings(String tcusername, String tcpassword, String externaluser, String careunitidtype,
+			String careunitid, String xml) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String rescheduleBooking(String tcusername, String tcpassword, String externaluser, String careunitidtype,
+			String careunitid, String xml) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String makeBooking(String tcusername, String tcpassword, String externaluser, String careunitidtype,
+			String careunitid, String xml) {
+		throw new UnsupportedOperationException();
+	}
+
+}
