064b7b369ee966551960920a95a5fcf5aa872f67
hans.thunberg <hans.thunberg@callistaenterprise.se>
2012-07-10 09:39:29 +0000

    Changed name to more soitoolkit style

---
+++ Components/EngagementindexPull/trunk/EngagementIndexPull/src/main/java/se/skl/skltpservices/pull/utils/DateHelper.java
@@ -0,0 +1,14 @@
+package se.skl.skltpservices.pull.utils;
+
+import java.util.Date;
+
+/**
+ * Author: Henrik Rostam
+ */
+public class DateHelper {
+
+    public static Date now() {
+        return new Date();
+    }
+
+}
