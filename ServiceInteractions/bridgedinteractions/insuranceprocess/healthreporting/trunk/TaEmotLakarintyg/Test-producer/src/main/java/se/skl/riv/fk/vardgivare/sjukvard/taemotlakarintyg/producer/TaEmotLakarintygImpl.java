f31014495b4ba4543083e5288622145fa3f5f599
hans.thunberg <hans.thunberg@callistaenterprise.se>
2012-09-01 20:23:05 +0000

    Removed systemout and add log4j logging

--- ServiceInteractions/bridgedinteractions/insuranceprocess/healthreporting/trunk/TaEmotLakarintyg/Test-producer/src/main/java/se/skl/riv/fk/vardgivare/sjukvard/taemotlakarintyg/producer/TaEmotLakarintygImpl.java
+++ ServiceInteractions/bridgedinteractions/insuranceprocess/healthreporting/trunk/TaEmotLakarintyg/Test-producer/src/main/java/se/skl/riv/fk/vardgivare/sjukvard/taemotlakarintyg/producer/TaEmotLakarintygImpl.java
@@ -22,30 +22,27 @@ package se.skl.riv.fk.vardgivare.sjukvard.taemotlakarintyg.producer;
 
 import javax.jws.WebService;
 
-import org.w3.wsaddressing10.AttributedURIType;
+import org.apache.cxf.ws.addressing.AttributedURIType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import se.fk.vardgivare.sjukvard.taemotlakarintyg.v1.rivtabp20.TaEmotLakarintygResponderInterface;
-import se.fk.vardgivare.sjukvard.taemotlakarintygresponder.v1.TaEmotLakarintygResponseType;
-import se.fk.vardgivare.sjukvard.taemotlakarintygresponder.v1.TaEmotLakarintygType;
-
-@WebService(
-		serviceName = "TaEmotLakarintygResponderService", 
-		endpointInterface="se.fk.vardgivare.sjukvard.taemotlakarintyg.v1.rivtabp20.TaEmotLakarintygResponderInterface", 
-		portName = "TaEmotLakarintygResponderPort", 
-		targetNamespace = "urn:riv:fk:vardgivare:sjukvard:TaEmotLakarintyg:1:rivtabp20",
-		wsdlLocation = "schemas/fk/TaEmotLakarintygInteraction_1.0_rivtabp20.wsdl")
+@WebService(serviceName = "TaEmotLakarintygResponderService", endpointInterface = "se.fk.vardgivare.sjukvard.taemotlakarintyg.v1.rivtabp20.TaEmotLakarintygResponderInterface", portName = "TaEmotLakarintygResponderPort", targetNamespace = "urn:riv:fk:vardgivare:sjukvard:TaEmotLakarintyg:1:rivtabp20", wsdlLocation = "schemas/fk/TaEmotLakarintygInteraction_1.0_rivtabp20.wsdl")
 public class TaEmotLakarintygImpl implements TaEmotLakarintygResponderInterface {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public TaEmotLakarintygResponseType taEmotLakarintyg(
 			AttributedURIType logicalAddress, TaEmotLakarintygType parameters) {
+
+		logger.info("taEmotLakarintyg({}, {})", logicalAddress.getValue(),
+				parameters);
+
 		try {
 			TaEmotLakarintygResponseType response = new TaEmotLakarintygResponseType();
-			
-			System.out.println("response sent!");
+			logger.info("response sent!");
 			return response;
 		} catch (RuntimeException e) {
-			System.out.println("Error occured: " + e);
-			throw e;
+			throw new RuntimeException("Error occured in taEmotLakarintyg", e);
 		}
 	}
 }
