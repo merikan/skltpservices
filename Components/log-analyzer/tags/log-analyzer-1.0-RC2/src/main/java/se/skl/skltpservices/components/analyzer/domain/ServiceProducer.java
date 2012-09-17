4bdec83cc308a462bb173309648b6dde2036e34b
peter.larsson <peter.larsson@callistaenterprise.se>
2012-09-17 07:54:42 +0000

    Minor updates handle endpoint property in log interface, and avoid jdbc errors when getting status for producers (active monitoring)

---
+++ Components/log-analyzer/tags/log-analyzer-1.0-RC2/src/main/java/se/skl/skltpservices/components/analyzer/domain/ServiceProducer.java
@@ -0,0 +1,49 @@
+package se.skl.skltpservices.components.analyzer.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ServiceProducer {
+    @Id private Long id;
+    private String serviceUrl;
+    private String domain;
+    private String subDomain;
+    private String system;
+    private String municipality;
+    
+    public Long getId() {
+        return id;
+    }
+    public String getServiceUrl() {
+        return serviceUrl;
+    }
+    public void setServiceUrl(String serviceUrl) {
+        this.serviceUrl = serviceUrl;
+    }
+    public String getDomain() {
+        return domain;
+    }
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+    public String getSubDomain() {
+        return subDomain;
+    }
+    public void setSubDomain(String subDomain) {
+        this.subDomain = subDomain;
+    }
+    public String getSystem() {
+        return system;
+    }
+    public void setSystem(String system) {
+        this.system = system;
+    }
+    public String getMunicipality() {
+        return municipality;
+    }
+    public void setMunicipality(String municiplaity) {
+        this.municipality = municiplaity;
+    }
+    
+}
