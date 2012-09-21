fafd96b1207130137381f6446fed47136b10e32c
peter.larsson@callistaenterprise.se <peter.larsson@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-09-21 12:03:49 +0000

    Minor update.

--- Components/log-analyzer/trunk/src/test/java/se/skl/skltpservices/components/analyzer/domain/ServiceProducerRepositoryTest.java
+++ Components/log-analyzer/trunk/src/test/java/se/skl/skltpservices/components/analyzer/domain/ServiceProducerRepositoryTest.java
@@ -2,6 +2,7 @@ package se.skl.skltpservices.components.analyzer.domain;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,17 +13,29 @@ public class ServiceProducerRepositoryTest extends TestSupport {
 	@Autowired
 	private ServiceProducerRepository repo;
 	
-	@Test
-	public void save() {
+	@Before
+	public void before() {
 		ServiceProducer sp = new ServiceProducer();
 		sp.setServiceUrl("http://slask");
 		sp.setDomainName("TP");
 		sp.setSystemName("APIGW");
-		sp.setDomainDescription("Nothing special to discuss");
+		sp.setDomainDescription("Nothing special to discuss ŒŠš");
 		sp.id = 1L;
 		repo.save(sp);
-		
+	}
+	
+	@Test
+	public void find() {
 		assertTrue(repo.findOne(1L) != null);
 	}
 	
+	@Test
+	public void validate() {
+		ServiceProducer sp = repo.findOne(1L);
+		assertEquals(sp.getServiceUrl(), "http://slask");
+		assertEquals(sp.getDomainName(), "TP");
+		assertEquals(sp.getSystemName(), "APIGW");
+		assertEquals(sp.getDomainDescription(), "Nothing special to discuss ŒŠš");
+	}
+
 }
