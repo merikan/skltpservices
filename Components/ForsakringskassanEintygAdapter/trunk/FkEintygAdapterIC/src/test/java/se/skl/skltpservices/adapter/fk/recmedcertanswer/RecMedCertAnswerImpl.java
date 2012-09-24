dbada38311ac51d815650bfcd2f1447121be167e
hans.thunberg <hans.thunberg@callistaenterprise.se>
2012-09-24 11:21:09 +0000

    Added a result text in the responses for testclass

--- Components/ForsakringskassanEintygAdapter/trunk/FkEintygAdapterIC/src/test/java/se/skl/skltpservices/adapter/fk/recmedcertanswer/RecMedCertAnswerImpl.java
+++ Components/ForsakringskassanEintygAdapter/trunk/FkEintygAdapterIC/src/test/java/se/skl/skltpservices/adapter/fk/recmedcertanswer/RecMedCertAnswerImpl.java
@@ -29,49 +29,57 @@ import org.w3.wsaddressing10.AttributedURIType;
 import se.skl.riv.insuranceprocess.healthreporting.receivemedicalcertificateanswer.v1.rivtabp20.ReceiveMedicalCertificateAnswerResponderInterface;
 import se.skl.riv.insuranceprocess.healthreporting.receivemedicalcertificateanswerresponder.v1.ReceiveMedicalCertificateAnswerResponseType;
 import se.skl.riv.insuranceprocess.healthreporting.receivemedicalcertificateanswerresponder.v1.ReceiveMedicalCertificateAnswerType;
+import se.skl.riv.insuranceprocess.healthreporting.v2.ErrorIdEnum;
 import se.skl.riv.insuranceprocess.healthreporting.v2.ResultCodeEnum;
 import se.skl.riv.insuranceprocess.healthreporting.v2.ResultOfCall;
 
 @WebService(serviceName = "ReceiveMedicalCertificateAnswerResponderService", endpointInterface = "se.skl.riv.insuranceprocess.healthreporting.receivemedicalcertificateanswer.v1.rivtabp20.ReceiveMedicalCertificateAnswerResponderInterface", portName = "ReceiveMedicalCertificateAnswerResponderPort", targetNamespace = "urn:riv:insuranceprocess:healthreporting:ReceiveMedicalCertificateAnswer:1:rivtabp20", wsdlLocation = "schemas/vard/interactions/ReceiveMedicalCertificateAnswerInteraction/ReceiveMedicalCertificateAnswerInteraction_1.0_rivtabp20.wsdl")
-public class RecMedCertAnswerImpl implements
-		ReceiveMedicalCertificateAnswerResponderInterface {
+public class RecMedCertAnswerImpl implements ReceiveMedicalCertificateAnswerResponderInterface {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	public ReceiveMedicalCertificateAnswerResponseType receiveMedicalCertificateAnswer(
-			AttributedURIType logicalAddress,
-			ReceiveMedicalCertificateAnswerType parameters) {
+			AttributedURIType logicalAddress, ReceiveMedicalCertificateAnswerType parameters) {
 
-		log.info("receiveMedicalCertificateAnswer({},{})", logicalAddress,
-				parameters);
+		log.info("receiveMedicalCertificateAnswer({},{})", logicalAddress.getValue(), parameters.getAnswer().getSvar()
+				.getMeddelandeText());
 
 		try {
 			ReceiveMedicalCertificateAnswerResponseType response = new ReceiveMedicalCertificateAnswerResponseType();
 
-			ResultOfCall resCall = new ResultOfCall();
-
-			String personId = parameters.getAnswer().getLakarutlatande()
-					.getPatient().getPersonId().getExtension();
+			String personId = parameters.getAnswer().getLakarutlatande().getPatient().getPersonId().getExtension();
 
 			log.info("Received personId: {}", personId);
 
 			// Check if to send an ERROR
-			if (parameters.getAnswer().getLakarutlatande().getPatient()
-					.getPersonId().getExtension()
+			if (parameters.getAnswer().getLakarutlatande().getPatient().getPersonId().getExtension()
 					.equalsIgnoreCase("19101010-1234")) {
-				resCall.setResultCode(ResultCodeEnum.ERROR);
-				response.setResult(resCall);
+				response.setResult(createErrorResultOfCall());
 			} else {
-				resCall.setResultCode(ResultCodeEnum.OK);
-				response.setResult(resCall);
+				response.setResult(createOKResultOfCall());
 			}
 
-			log.info("Response returned: {} ", resCall.getResultCode());
+			log.info("Response returned: {} {}", response.getResult().getResultCode());
 
 			return response;
 		} catch (RuntimeException e) {
 			log.error("Error occured: " + e);
 			throw e;
 		}
+	}
+
+	private ResultOfCall createErrorResultOfCall() {
+		ResultOfCall resultOfCall = new ResultOfCall();
+		resultOfCall.setResultCode(ResultCodeEnum.ERROR);
+		resultOfCall.setErrorId(ErrorIdEnum.VALIDATION_ERROR);
+		resultOfCall.setErrorText("It went wrong");
+		return null;
+	}
+
+	private ResultOfCall createOKResultOfCall() {
+		ResultOfCall resultOfCall = new ResultOfCall();
+		resultOfCall.setInfoText("It went ok");
+		resultOfCall.setResultCode(ResultCodeEnum.OK);
+		return resultOfCall;
 	}
 }
