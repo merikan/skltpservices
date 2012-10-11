882f81f472225387b541716037c53e353c88493e
hans.thunberg <hans.thunberg@callistaenterprise.se>
2012-10-11 20:13:04 +0000

    Updated testdata to match testproducer url

--- Components/Supervisor/trunk/VpSupervisorIntegrationComponent/src/environment/setup/skltpSupervisor-db-insert-testdata.sql
+++ Components/Supervisor/trunk/VpSupervisorIntegrationComponent/src/environment/setup/skltpSupervisor-db-insert-testdata.sql
@@ -1,1 +1,1 @@
-insert into ServiceProducer values('1', 'http://example.com/path/to/service', 'Tidbok', 'Tidbokning', 'Kalendercentralen');
+insert into ServiceProducer values('1', 'https://localhost:20001/supervisor/PingForConfiguration/1/rivtabp21', 'Tidbok', 'Tidbokning', 'Local testproducer');
