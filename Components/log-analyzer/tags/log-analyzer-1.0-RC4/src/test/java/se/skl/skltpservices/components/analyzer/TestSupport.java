d9d49cf5047f660c904dc287e63894f8d36871e8
peter.larsson@callistaenterprise.se <peter.larsson@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2012-12-15 20:12:22 +0000

    Activity indicator fix

---
+++ Components/log-analyzer/tags/log-analyzer-1.0-RC4/src/test/java/se/skl/skltpservices/components/analyzer/TestSupport.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2012, Sjukvardsradgivningen. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package se.skl.skltpservices.components.analyzer;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath*:applicationContext.xml")
+public abstract class TestSupport {
+
+}
