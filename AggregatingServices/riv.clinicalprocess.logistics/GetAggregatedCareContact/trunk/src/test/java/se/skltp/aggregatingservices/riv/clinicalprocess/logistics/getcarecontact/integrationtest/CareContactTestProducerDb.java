f0563537bcad35fa8a8d1ab12dd4f4bdcaaf372f
albert.orwall <albert.orwall@callistaenterprise.se>
2013-03-14 09:52:32 +0000

    Aggregated service for GetCareContact

---
+++ AggregatingServices/riv.clinicalprocess.logistics/GetAggregatedCareContact/trunk/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/getcarecontact/integrationtest/CareContactTestProducerDb.java
@@ -0,0 +1,69 @@
+package se.skltp.aggregatingservices.riv.clinicalprocess.logistics.getcarecontact.integrationtest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.riv.clinicalprocess.logistics.getcarecontactresponder.v2.GetCareContactResponseType;
+import se.riv.clinicalprocess.logistics.v2.AuthorType;
+import se.riv.clinicalprocess.logistics.v2.CareContactBodyType;
+import se.riv.clinicalprocess.logistics.v2.CareContactType;
+import se.riv.clinicalprocess.logistics.v2.CareContactUnitType;
+import se.riv.clinicalprocess.logistics.v2.PatientIdType;
+import se.riv.clinicalprocess.logistics.v2.PatientSummaryHeaderType;
+import se.skltp.agp.test.producer.TestProducerDb;
+
+public class CareContactTestProducerDb extends TestProducerDb {
+
+    private static final Logger log = LoggerFactory.getLogger(CareContactTestProducerDb.class);
+
+    @Override
+    public Object createResponse(Object... responseItems) {
+        log.debug("Creates a response with {} items", responseItems);
+        GetCareContactResponseType response = new GetCareContactResponseType();
+        for (int i = 0; i < responseItems.length; i++) {
+            response.getCareContact().add((CareContactType)responseItems[i]);
+        }
+        return response;
+    }
+
+    public static final String TEST_REASON_DEFAULT = "default reason";
+    public static final String TEST_REASON_UPDATED = "updated reason";
+
+    @Override
+    public Object createResponseItem(String logicalAddress, String registeredResidentId, String businessObjectId) {
+
+        log.debug("Created one response item for logical-address {}, registeredResidentId {} and businessObjectId {}",
+                new Object[] {logicalAddress, registeredResidentId, businessObjectId});
+
+        CareContactType response = new CareContactType();
+        PatientSummaryHeaderType header = new PatientSummaryHeaderType();
+        PatientIdType patientId = new PatientIdType();
+        patientId.setId(registeredResidentId);
+        patientId.setType("1.2.752.129.2.1.3.1");
+        header.setPatientId(patientId);
+        header.setApprovedForPatient(true);
+        header.setSourceSystem(logicalAddress);
+        header.setDocumentTitle("dokumenttitel");
+        header.setDocumentTime("20130302120101");
+        AuthorType author = new AuthorType();
+        author.setCareUnitHSAid(logicalAddress);
+        header.setAuthor(author);
+        header.setDocumentId(businessObjectId);
+        response.setCareContactHeader(header);
+
+        CareContactBodyType body = new CareContactBodyType();
+        body.setCareContactCode(0);
+        body.setCareContactReason("reason");
+        body.setCareContactStatus(0);
+        body.setCareContactTime("20130213121419");
+        
+        CareContactUnitType unit = new CareContactUnitType();
+        unit.setCareContactUnitAddress("address");
+        unit.setCareContactUnitId("unitId");
+        unit.setCareContactUnitName("name");
+        
+        body.getCareContactUnit().add(unit);
+        response.setCareContactBody(body);
+        return response;
+    }
+}
