7d0ac98d71e78fd977b61159d6e62057b422007d
hans.thunberg <hans.thunberg@callistaenterprise.se>
2013-04-23 06:13:46 +0000

    Updated testconsumer to be able to run load tests on SKLTP VP

--- ServiceInteractions/riv/itinfra/tp/trunk/Ping/Test-consumer/src/main/java/se/riv/itinfra/tp/ping/PingConsumer.java
+++ ServiceInteractions/riv/itinfra/tp/trunk/Ping/Test-consumer/src/main/java/se/riv/itinfra/tp/ping/PingConsumer.java
@@ -20,9 +20,20 @@
  */
 package se.riv.itinfra.tp.ping;
 
-import java.net.MalformedURLException;
+import java.io.FileInputStream;
 import java.net.URL;
+import java.security.KeyStore;
+import java.util.Random;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManagerFactory;
+import javax.xml.ws.BindingProvider;
+
+import org.apache.cxf.configuration.jsse.TLSClientParameters;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 import org.w3.wsaddressing10.AttributedURIType;
 
 import se.riv.itinfra.tp.ping.v1.rivtabp20.PingResponderInterface;
@@ -32,46 +43,56 @@ import se.riv.itinfra.tp.pingresponder.v1.PingResponseType;
 
 public final class PingConsumer {
 
-	// Use this one to connect via Virtualiseringsplattformen
-	private static final String LOGISK_ADDRESS = "/Ping_Service";
-	// Use this one to connect directly (just for test)
+	private static String logicalAddress = "Ping";
+	private static long sleepTime = 5000L;
+	private static String endpointAddress = "https://localhost:20000/vp/Ping/1/rivtabp20";
 
-	public static void main(String[] args) {
-		String host = "localhost:10000/test";
+	private static PingResponderInterface service = null;
+
+	public static void main(String[] args) throws Exception {
+
 		if (args.length > 0) {
-			host = args[0];
+			sleepTime = Long.parseLong(args[0]);
+			endpointAddress = args[1];
+			logicalAddress = args[2];
 		}
 
-		// Setup ssl info for the initial ?wsdl lookup...
-		System.setProperty("javax.net.ssl.keyStore","../../certs/consumer.jks");
-		System.setProperty("javax.net.ssl.keyStorePassword", "password");
-		System.setProperty("javax.net.ssl.keyStoreType", "jks");
-		System.setProperty("javax.net.ssl.trustStore", "../../certs/truststore.jks");
-		System.setProperty("javax.net.ssl.trustStorePassword", "password");
+		long randomness = sleepTime / 3L;
+		Random random = new Random();
+		System.out.println("Consumer connecting to " + endpointAddress);
+		service = setupService(endpointAddress, logicalAddress);
+		while (true) {
+			long start = System.nanoTime();
+			try {
+				String p = callService(logicalAddress);
+				System.out.println("Returned: " + p);
+			} catch (Exception e) {
+				System.out.println("Error: " + e.toString());
+			} finally {
+				System.out.println("Total time: " + (System.nanoTime() - start)
+						/ 1000000L + " ms");
+			}
+			Thread.sleep((long) (sleepTime - randomness + 2L * randomness
+					* random.nextDouble()));
+		}
 
-		String adress = "https://" + host + LOGISK_ADDRESS;
-		System.out.println("Consumer connecting to "  + adress);
-		String p = callService(adress, "Test");
-		System.out.println("Returned: " + p);
 	}
 
-	public static String callService(String serviceAddress, String logicalAddresss) {
-		
-		PingResponderInterface service = new PingResponderService(
-			createEndpointUrlFromServiceAddress(serviceAddress)).getPingResponderPort();
+	public static String callService(String logicalAddresss) {
 
 		AttributedURIType logicalAddressHeader = new AttributedURIType();
 		logicalAddressHeader.setValue(logicalAddresss);
 
 		PingRequestType request = new PingRequestType();
-		
+
 		// The below request is created to mimic the sample xml-message
 		request.setPingIn("Pinging!");
-	
-		try {
-		    PingResponseType result = service.ping(logicalAddressHeader, request);
 
-		    return ("Ping response=" + result.getPingUt());
+		try {
+			PingResponseType result = service.ping(logicalAddressHeader,
+					request);
+
+			return ("Ping response=" + result.getPingUt());
 
 		} catch (Exception ex) {
 			System.out.println("Exception=" + ex.getMessage());
@@ -79,11 +100,74 @@ public final class PingConsumer {
 		}
 	}
 
-	public static URL createEndpointUrlFromServiceAddress(String serviceAddress) {
+	public static PingResponderInterface setupService(String serviceAddress,
+			String logicalAddresss) {
 		try {
-			return new URL(serviceAddress + "?wsdl");
-		} catch (MalformedURLException e) {
-			throw new RuntimeException("Malformed URL Exception: " + e.getMessage());
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			URL wsdlUrl = loader
+					.getResource("PingInteraction_1.0_rivtabp20.wsdl");
+
+			PingResponderService service = new PingResponderService(wsdlUrl);
+			PingResponderInterface serviceInterface = service
+					.getPingResponderPort();
+
+			BindingProvider provider = (BindingProvider) serviceInterface;
+			provider.getRequestContext().put(
+					"javax.xml.ws.service.endpoint.address", serviceAddress);
+
+			Client client = ClientProxy.getClient(serviceInterface);
+			HTTPConduit http = (HTTPConduit) client.getConduit();
+
+			HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
+			httpClientPolicy.setConnectionTimeout(36000L);
+			httpClientPolicy.setAllowChunking(false);
+			httpClientPolicy.setReceiveTimeout(32000L);
+			http.setClient(httpClientPolicy);
+
+			if (serviceAddress.startsWith("https")) {
+				TLSClientParameters tlsCP = setUpTlsClientParams();
+				http.setTlsClientParameters(tlsCP);
+			}
+
+			return serviceInterface;
+		} catch (Throwable ex) {
+			System.out.println("Exception={}" + ex.getMessage());
+			throw new RuntimeException(ex);
 		}
+	}
+
+	private static TLSClientParameters setUpTlsClientParams() throws Exception {
+
+		KeyStore trustStore = KeyStore.getInstance("JKS");
+		String trustStoreLoc = "src/main/resources/test-certs/truststore.jks";
+		String trustPassword = "password";
+		trustStore.load(new FileInputStream(trustStoreLoc),
+				trustPassword.toCharArray());
+
+		String keyPassword = "password";
+		KeyStore keyStore = KeyStore.getInstance("jks");
+		String keyStoreLoc = "src/main/resources/test-certs/client.jks";
+		keyStore.load(new FileInputStream(keyStoreLoc),
+				keyPassword.toCharArray());
+
+		TrustManagerFactory tmf = TrustManagerFactory
+				.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		tmf.init(trustStore);
+
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory
+				.getDefaultAlgorithm());
+		kmf.init(keyStore, keyPassword.toCharArray());
+
+		TLSClientParameters tlsCP = new TLSClientParameters();
+		tlsCP.setTrustManagers(tmf.getTrustManagers());
+		tlsCP.setKeyManagers(kmf.getKeyManagers());
+
+		// The following is not recommended and would not be done in a
+		// prodcution environment,
+		// this is just for illustrative purpose
+		tlsCP.setDisableCNCheck(true);
+
+		return tlsCP;
+
 	}
 }
