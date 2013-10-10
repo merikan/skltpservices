a136ed34c15ff61972494309f28f186c79e19b43
jonas.behmer@callistaenterprise.se <jonas.behmer@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2013-10-10 07:15:58 +0000

    SKLTP-290 Utveckla funktionella tester aggregerande tjänst tidbokning: Nya testproducenter + tillägg av groovy för att skapa dessa testproducenter.

---
+++ ServiceInteractions/riv/crm/scheduling/trunk/GetSubjectOfCareSchedule/Test-producer/src/main/groovy/se/riv/crm/scheduling/getsubjectofcareschedule/v1/GetSubjectOfCareScheduleProducer2.groovy
@@ -0,0 +1,61 @@
+package se.riv.crm.scheduling.getsubjectofcareschedule.v1;
+
+import java.text.SimpleDateFormat;
+
+import javax.jws.WebService;
+
+import riv.interoperability.headers._1.ActorType;
+import se.riv.crm.scheduling.getsubjectofcareschedule.v1.rivtabp21.GetSubjectOfCareScheduleResponderInterface;
+import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.GetSubjectOfCareScheduleResponseType;
+import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.GetSubjectOfCareScheduleType;
+import se.riv.crm.scheduling.v1.TimeslotType;
+
+@WebService(
+		serviceName = "GetSubjectOfCareScheduleResponderService", 
+		endpointInterface="se.riv.crm.scheduling.getsubjectofcareschedule.v1.rivtabp21.GetSubjectOfCareScheduleResponderInterface", 
+		portName = "GetSubjectOfCareScheduleResponderPort", 
+		targetNamespace = "urn:riv:crm:scheduling:GetSubjectOfCareSchedule:1:rivtabp21",
+		wsdlLocation = "interactions/GetSubjectOfCareScheduleInteraction/GetSubjectOfCareScheduleInteraction_1.1_RIVTABP21.wsdl")
+public class GetSubjectOfCareScheduleProducer2 implements GetSubjectOfCareScheduleResponderInterface {
+	
+	private final static String ADAM = "195609230577"
+	private final static String HSAPRODUCER2 = "HSAPRODUCER2"
+	
+	@Override
+	public GetSubjectOfCareScheduleResponseType getSubjectOfCareSchedule(
+			String logicalAddress, ActorType actor,
+			GetSubjectOfCareScheduleType request) {
+		
+			def response = new GetSubjectOfCareScheduleResponseType() //Always return a response of this type, never return null
+			println "### Virtual service for GetSubjectOfCareSchedule call the source system with logical address: $logicalAddress and patientId: ${request.getSubjectOfCare()}"
+			
+			if(request.subjectOfCare == ADAM) {
+				//Engagemang 1
+				def timeslot = new TimeslotType()
+				timeslot.with {
+					bookingId = "PRODUCER2_BookingID_Adam_1"
+					startTimeInclusive = "20131212100145"
+					endTimeExclusive = "201312121050145"
+					healthcareFacility = HSAPRODUCER2
+					subjectOfCare = ADAM
+				}
+				response.timeslotDetail << timeslot
+				
+				//Engagemang 2
+				timeslot = new TimeslotType()
+				timeslot.with {
+					bookingId = "PRODUCER2_BookingID_Adam_2"
+					startTimeInclusive = "20140123093000"
+					endTimeExclusive = "20140123100000"
+					healthcareFacility = HSAPRODUCER2
+					subjectOfCare = ADAM
+				}
+				response.timeslotDetail << timeslot
+			}
+			
+			println "### Virtual service got ${response.getTimeslotDetail().size()} booknings in the reply from the source system with logical address: $logicalAddress and patientId: ${request.subjectOfCare}"
+	
+			// We are done
+			return response
+	}
+}
