e29ae4af1c93b12af7570f0c31cc64872bbb59dc
hans.thunberg <hans.thunberg@callistaenterprise.se>
2013-11-27 15:41:08 +0000

    [maven-release-plugin]  copy for tag TicketMachine-1.1.0-RC3

---
+++ AdapterServices/riv.se_apotekensservice/TicketMachine/tags/TicketMachine-1.1.0-RC3/src/main/java/se/inera/pascal/ticket/core/ApseAuthorizationAttributes.java
@@ -0,0 +1,185 @@
+package se.inera.pascal.ticket.core;
+
+public class ApseAuthorizationAttributes {
+	private String rollnamn = "";
+	private String katalogId = "";
+	private String katalog = "";
+	private String forskrivarkod = "";
+	private String legitimationskod = "";
+	private String yrkeskod = "";
+	private String befattningskod = "";
+	private String fornamn = "";
+	private String efternamn = "";
+	private String arbetsplatskod = "";
+	private String arbetsplats = "";
+	private String postadress = "";
+	private String postnummer = "";
+	private String postort = "";
+	private String telefonnummer = "";
+	private String personnummer = "";
+	private String organisationsnummer = "";
+	private String roll = ""; //Se getter och setter för kommentar kring detta fälts relation med rollnamn.
+
+	public ApseAuthorizationAttributes() {
+	}
+
+	public void setRollnamn(String rollnamn) {
+		this.rollnamn = rollnamn;
+	}
+
+	public String getRollnamn() {
+		return rollnamn;
+	}
+
+	public void setKatalogId(String katalogId) {
+		this.katalogId = katalogId;
+	}
+
+	public String getKatalogId() {
+		return katalogId;
+	}
+
+	public void setKatalog(String katalog) {
+		this.katalog = katalog;
+	}
+
+	public String getKatalog() {
+		return katalog;
+	}
+
+	public void setForskrivarkod(String forskrivarkod) {
+		this.forskrivarkod = forskrivarkod;
+	}
+
+	public String getForskrivarkod() {
+		return forskrivarkod;
+	}
+
+	public void setLegitimationskod(String legitimationskod) {
+		this.legitimationskod = legitimationskod;
+	}
+
+	public String getLegitimationskod() {
+		return legitimationskod;
+	}
+
+	public void setYrkeskod(String yrkeskod) {
+		this.yrkeskod = yrkeskod;
+	}
+
+	public String getYrkeskod() {
+		return yrkeskod;
+	}
+
+	public void setBefattningskod(String befattningskod) {
+		this.befattningskod = befattningskod;
+	}
+
+	public String getBefattningskod() {
+		return befattningskod;
+	}
+
+	public void setFornamn(String fornamn) {
+		this.fornamn = fornamn;
+	}
+
+	public String getFornamn() {
+		return fornamn;
+	}
+
+	public void setEfternamn(String efternamn) {
+		this.efternamn = efternamn;
+	}
+
+	public String getEfternamn() {
+		return efternamn;
+	}
+
+	public void setArbetsplatskod(String arbetsplatskod) {
+		this.arbetsplatskod = arbetsplatskod;
+	}
+
+	public String getArbetsplatskod() {
+		return arbetsplatskod;
+	}
+
+	public void setArbetsplats(String arbetsplats) {
+		this.arbetsplats = arbetsplats;
+	}
+
+	public String getArbetsplats() {
+		return arbetsplats;
+	}
+
+	public void setPostadress(String postadress) {
+		this.postadress = postadress;
+	}
+
+	public String getPostadress() {
+		return postadress;
+	}
+
+	public void setPostnummer(String postnummer) {
+		this.postnummer = postnummer;
+	}
+
+	public String getPostnummer() {
+		return postnummer;
+	}
+
+	public void setPostort(String postort) {
+		this.postort = postort;
+	}
+
+	public String getPostort() {
+		return postort;
+	}
+
+	public void setTelefonnummer(String telefonnummer) {
+		this.telefonnummer = telefonnummer;
+	}
+
+	public String getTelefonnummer() {
+		return telefonnummer;
+	}
+	
+	public String getPersonnummer() {
+		return personnummer;
+	}
+	
+	public void setPersonnummer(String personnummer) {
+		this.personnummer = personnummer;
+	}
+
+	public void setOrganisationsnummer(String organisationsnummer) {
+		this.organisationsnummer = organisationsnummer; 
+	}
+	
+	public String getOrganisationsnummer() {
+		return organisationsnummer;
+	}
+
+	/**
+	 * Obs! Roll är det attribut i säkerhetsheadern som ska användas enligt dokumentet
+	 * Säkerhetsheader v1.0 från Apotekens service. Vi behåller dock det parallella spåret
+	 * med rollnamn för organisationsbiljetter för att befintlig kod i produktion ej ska drabbas.
+	 * Se https://skl-tp.atlassian.net/browse/SKLTP-346
+	 * 
+	 * @param roll
+	 */
+	public void setRoll(String roll) {
+		this.roll = roll;
+	}
+	
+	/**
+	 * Obs! Roll är det attribut i säkerhetsheadern som ska användas enligt dokumentet
+	 * Säkerhetsheader v1.0 från Apotekens service. Vi behåller dock det parallella spåret
+	 * med rollnamn för organisationsbiljetter för att befintlig kod i produktion ej ska drabbas.
+	 * Se https://skl-tp.atlassian.net/browse/SKLTP-346
+	 * 
+	 * @return roll
+	 */
+	public String getRoll() {
+		return roll;
+	}
+}
---
+++ AdapterServices/riv.se_apotekensservice/TicketMachine/tags/TicketMachine-1.1.0-RC3/src/test/java/se/inera/pascal/ticket/ArgosTicketTest.java
@@ -0,0 +1,313 @@
+package se.inera.pascal.ticket;
+
+import static java.lang.String.format;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ArgosTicketTest {
+	private static String samlAttributeTemplate = "<saml2:Attribute Name=\"%s\" NameFormat=\"urn:oasis:names:tc:SAML:2.0:attrname-format:uri\"><saml2:AttributeValue xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:type=\"xs:string\">%s</saml2:AttributeValue></saml2:Attribute>";
+	
+	@Test
+	public void happyCaseToSeeHowTicketGeneratorWorks() {
+
+		String forskrivarkod = "1111152";
+		String legitimationskod = "1111111";
+		String fornamn = "Lars";
+		String efternamn = "LÂkare";
+		String yrkesgrupp = "FORSKRIVARE";
+		String befattningskod = "1111111";
+		String arbetsplatskod = "4000000000001";
+		String arbetsplatsnamn = "VC Test";
+		String postort = "Staden";
+		String postadress = "VÂgen 1";
+		String postnummer = "11111";
+		String telefonnummer = "0987654321";
+		String requestId = "12345676";
+		String rollnamn = "LK";
+		// String directoryID = "SE1111111111-1003";
+		String hsaID = "SE1111111111-1003";
+		String katalog = "HSA";
+		String organisationsnummer = "111111111";
+		String systemnamn = "Pascal";
+		String systemversion = "1.0";
+		String systemIp = "192.168.1.1";
+
+		String ticket = new ArgosTicket().getTicketForOrganization(forskrivarkod, legitimationskod, fornamn, efternamn, yrkesgrupp,
+				befattningskod, arbetsplatskod, arbetsplatsnamn, postort, postadress, postnummer, telefonnummer,
+				requestId, rollnamn, hsaID, katalog, organisationsnummer, systemnamn, systemversion, systemIp);
+
+		assertThat(ticket, containsString("<saml2:Issuer>pascalonline</saml2:Issuer>"));
+		
+		//Check that attributes used in tickets for individuals doesn't show up here
+		//It is just a precaution as we don't want any conflicts with what's working in production today.
+		assertThat(ticket, not(containsString("urn:apotekensservice:names:federation:attributeName:organisationsnummer")));
+		assertThat(ticket, not(containsString("urn:apotekensservice:names:federation:attributeName:personnummer")));
+		assertThat(ticket, not(containsString("urn:apotekensservice:names:federation:attributeName:roll\""))); //as we might match the attributeNamen rollnamn I had to add the little extra \"		
+	}
+
+	@Test
+	public void personel_ticket_used_in_a_static_way() {
+
+		final ArgosTicket argosTicket = new ArgosTicket();
+		String forskrivarkod = "1111152";
+		String legitimationskod = "1111111";
+		String fornamn = "Lars";
+		String efternamn = "LÂkare";
+		String yrkesgrupp = "FORSKRIVARE";
+		String befattningskod = "1111111";
+		String arbetsplatskod = "4000000000001";
+		String arbetsplatsnamn = "VC Test";
+		String postort = "Staden";
+		String postadress = "VÂgen 1";
+		String postnummer = "11111";
+		String telefonnummer = "0987654321";
+		String requestId = "12345676";
+		String rollnamn = "LK";
+		// String directoryID = "SE1111111111-1003";
+		String hsaID = "SE1111111111-1003";
+		String katalog = "HSA";
+		String organisationsnummer = "111111111";
+		String systemnamn = "Pascal";
+		String systemversion = "1.0";
+		String systemIp = "192.168.1.1";
+
+		String ticket = argosTicket.getTicketForOrganization(forskrivarkod, legitimationskod, fornamn, efternamn, yrkesgrupp,
+				befattningskod, arbetsplatskod, arbetsplatsnamn, postort, postadress, postnummer, telefonnummer,
+				requestId, rollnamn, hsaID, katalog, organisationsnummer, systemnamn, systemversion, systemIp);
+		
+		ticket = argosTicket.getTicketForOrganization(null, null, null, null, null, null, null,
+				null, null, null, null, null, null, null, null, null, null, null, null, null);
+		
+		assertThat(ticket, not(containsString(forskrivarkod)));
+		assertThat(ticket, not(containsString(legitimationskod)));
+		assertThat(ticket, not(containsString(fornamn)));
+		assertThat(ticket, not(containsString(efternamn)));
+		assertThat(ticket, not(containsString(yrkesgrupp)));
+		assertThat(ticket, not(containsString(befattningskod)));
+		assertThat(ticket, not(containsString(arbetsplatskod)));
+		assertThat(ticket, not(containsString(arbetsplatsnamn)));
+		assertThat(ticket, not(containsString(postort)));
+		assertThat(ticket, not(containsString(postadress)));
+		assertThat(ticket, not(containsString(postnummer)));
+		assertThat(ticket, not(containsString(telefonnummer)));
+		assertThat(ticket, not(containsString(requestId)));
+		assertThat(ticket, not(containsString(rollnamn)));
+		assertThat(ticket, not(containsString(hsaID)));
+		assertThat(ticket, not(containsString(">" + katalog + "<")));
+		assertThat(ticket, not(containsString(organisationsnummer)));
+		assertThat(ticket, not(containsString(">" + systemnamn + "<")));
+		assertThat(ticket, not(containsString(systemversion)));
+		assertThat(ticket, not(containsString(systemIp)));
+		
+	}
+	
+	@Test
+	public void ineraCodeDoesNotGiveErrorWhenOneFieldIsNull() {
+
+		String forskrivarkod = "1111129";
+		String legitimationskod = null;
+		String fornamn = "Lars";
+		String efternamn = "LÂkare";
+		String yrkesgrupp = "LÂkare";
+		String befattningskod = "123456";
+		String arbetsplatskod = "1234567890";
+		String arbetsplatsnamn = "Sjukhuset";
+		String postort = "Staden";
+		String postadress = "VÂgen 1";
+		String postnummer = "11111";
+		String telefonnummer = "08-1234567";
+		String requestId = "123456";
+		String rollnamn = "FORSKRIVARE";
+		String directoryID = "TSE6565656565-1003";
+		String hsaID = "TSE6565656565-1003";
+		String katalog = "HSA";
+		String organisationsnummer = "1234567890";
+		String systemnamn = "Melior";
+		String systemversion = "1.0";
+		String systemIp = "192.0.0.1";
+
+		String ticket = new ArgosTicket().getTicketForOrganization(forskrivarkod, legitimationskod, fornamn, efternamn, yrkesgrupp,
+				befattningskod, arbetsplatskod, arbetsplatsnamn, postort, postadress, postnummer, telefonnummer,
+				requestId, rollnamn, hsaID, katalog, organisationsnummer, systemnamn, systemversion, systemIp);
+
+		assertNotNull(ticket);
+		
+	}
+	
+	@Test
+	public void a_ticket_for_usage_in_individual_based_services_can_be_produced() {
+
+		//given
+		String fornamn = "Lars";
+		String efternamn = "Larsson";
+		String rollnamn = "PRIVATPERSON";
+		String personnummer = "197109231234";
+		String organisationsnummer = "MVK_111111111";
+		String requestId = "12345676";
+		String systemIp = "192.168.1.1";
+		String systemnamn = "Mina vardkontakter";
+		String systemversion = "1.0";
+		String ticket = new ArgosTicket().getTicketForCitizen(fornamn, efternamn, personnummer, rollnamn, organisationsnummer, requestId, systemIp, systemnamn, systemversion) ;
+		
+		
+		assertsForCitizenTicket(fornamn, efternamn, rollnamn, personnummer,
+				organisationsnummer, requestId, systemIp, systemnamn,
+				systemversion, ticket);
+	}
+	
+	@Test
+	public void a_ticket_for_usage_in_individual_based_services_used_in_a_static_context_does_not_cache_values() {
+		
+		//An ArgosTicket that we will reuse, simulating how clients use it in a static context. This should not cache values.
+		final ArgosTicket argosTicket = new ArgosTicket();
+		
+		//given
+		String fornamn = "Lars";
+		String efternamn = "Larsson";
+		String rollnamn = "PRIVATPERSON";
+		String personnummer = "197109231234";
+		String organisationsnummer = "MVK_111111111";
+		String requestId = "12345676";
+		String systemIp = "192.168.1.1";
+		String systemnamn = "Mina vardkontakter";
+		String systemversion = "1.0";
+		String ticket = argosTicket.getTicketForCitizen(fornamn, efternamn, personnummer, rollnamn, organisationsnummer, requestId, systemIp, systemnamn, systemversion) ;
+		
+		ticket = argosTicket.getTicketForCitizen(null, null, null, null, null, null, null, null, null);
+
+		assertThat(ticket, not(containsString(fornamn)));
+		assertThat(ticket, not(containsString(efternamn)));
+		assertThat(ticket, not(containsString(personnummer)));
+		assertThat(ticket, not(containsString(organisationsnummer)));
+		assertThat(ticket, not(containsString(rollnamn)));
+		assertThat(ticket, not(containsString(requestId)));
+		assertThat(ticket, not(containsString(systemversion)));
+		assertThat(ticket, not(containsString(systemIp)));
+	}
+
+	private void assertsForCitizenTicket(String fornamn, String efternamn,
+			String rollnamn, String personnummer, String organisationsnummer,
+			String requestId, String systemIp, String systemnamn,
+			String systemversion, String ticket) {
+		//Autentiseringsintyget
+		//directoryId
+		String expectedDirectoryIDSamlAttribute = format(samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:DirectoryID",
+				personnummer);
+		assertThat("Ticket should contain directoryID and personnummer", ticket, containsString(expectedDirectoryIDSamlAttribute));
+		
+		//organizationId
+		String expectedOrganizationIdSamlAttribute = format(samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:OrganizationID",
+				organisationsnummer);
+		assertThat("Ticket should contain organizationID and Organisationsnummer",
+				ticket, containsString(expectedOrganizationIdSamlAttribute));
+		
+		//Auktorisationsintyget
+		//assertionType mÃ¥ste innehÃ¥lla vÃ¤rdet AuthorizationData
+		String expectedAssertionTypeSamlAttribute = format(samlAttributeTemplate, "urn:apotekensservice:names:federation:attributeName:assertionType", "AuthorizationData");
+		assertThat("Ticket should contain urn for assertionType and the value AuthorizationData",
+				ticket, containsString(expectedAssertionTypeSamlAttribute));
+
+		//connectedAssertionId
+		assertThat("Ticket should contain urn for connectedAssertionID",
+				ticket, containsString("urn:apotekensservice:names:federation:attributeName:connectedAssertionId"));
+		assertNotNull("connectedAssertionID was not found in ticket", getConnectedAssertionIDFromTicket(ticket));
+		
+		//efternamn
+		String expectedEfternamnSamlAttribute = format(samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:efternamn",
+				efternamn);
+		assertThat("Ticket should contain attribute efternamn and its attributevalue",
+				ticket, containsString(expectedEfternamnSamlAttribute));
+
+		//fÃ¶rnamn
+		String expectedFornamnSamlAttribute = format(samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:fornamn",
+				fornamn);
+		assertThat("Ticket should contain attribute fornamn and its attributevalue",
+				ticket, containsString(expectedFornamnSamlAttribute));
+		
+		//organisationsnummer
+		String expectedOrganisationsnummerSamlAttribute = format(samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:organisationsnummer",
+				organisationsnummer);
+		assertThat("Ticket should contain attribute organisationsnummer and its attributevalue",
+				ticket, containsString(expectedOrganisationsnummerSamlAttribute));
+		
+		//roll ska förekomma, inte rollnamn se https://skl-tp.atlassian.net/browse/SKLTP-346
+		String expectedRollSamlAttribute = format(samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:roll",
+				rollnamn);
+		assertThat("Ticket should contain attribute roll and its attributevalue PRIVATPERSON",
+				ticket, containsString(expectedRollSamlAttribute));
+		
+		assertThat(ticket, not(containsString("urn:apotekensservice:names:federation:attributeName:rollnamn")));
+		
+		//Informationsintyget
+		//systemIp
+		String expectedSystemIPSamlAttribute = format(samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:systemIp",
+				systemIp);
+		assertThat("Ticket should contain attribute systemIp and its attributevalue",
+				ticket, containsString(expectedSystemIPSamlAttribute));
+		
+		//systemnamn
+		String expectedSystemNamnSamlAttribute = format(samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:systemnamn",
+				systemnamn);
+		assertThat("Ticket should contain attribute systemnamn and its attributevalue",
+				ticket, containsString(expectedSystemNamnSamlAttribute));
+		
+		//systemversion
+		String expectedSystemVersionSamlAttribute = format(
+				samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:systemversion",
+				systemversion);
+		assertThat(
+				"Ticket should contain attribute systemversion and its attributevalue",
+				ticket, containsString(expectedSystemVersionSamlAttribute));
+		
+		//requestId
+		String expectedRequestIDSamlAttribute = format(
+				samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:requestId",
+				requestId);
+		assertThat(
+				"Ticket should contain attribute requestId and its attributevalue",
+				ticket, containsString(expectedRequestIDSamlAttribute));
+		
+		// assertionType mÃ¥ste innehÃ¥lla vÃ¤rdet AuthorizationData
+		String expectedAssertionTypeInfoDataSamlAttribute = format(
+				samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:assertionType",
+				"InfoData");
+		assertThat(
+				"Ticket should contain urn for assertionType and the value InfoData",
+				ticket, containsString(expectedAssertionTypeInfoDataSamlAttribute));
+	}
+	
+	private String getConnectedAssertionIDFromTicket(String ticket) {
+		String connectedAssertionId = null;
+		
+		String connectedAssertionIDRegexPattern = format(samlAttributeTemplate,
+				"urn:apotekensservice:names:federation:attributeName:connectedAssertionId",
+				"(_[a-z,A-Z,0-9]*)");
+		
+		Pattern pattern = Pattern.compile(connectedAssertionIDRegexPattern);
+		Matcher matcher = pattern.matcher(ticket);
+		if(matcher.find()){
+			connectedAssertionId = matcher.group(1);
+		}
+		
+		return connectedAssertionId;
+	}
+}
