2e21e7edf1021b83c1eb03e919ea7a8e5a8a47ce
magnus.larsson@callistaenterprise.se <magnus.larsson@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2014-02-03 16:27:29 +0000

    ApSeRetryAdapter: 1. Retry component is now without hardcoded properties, all in property-file + spring injection 2. Duplicated code removed in Retry component

--- AdapterServices/riv.druglogistics.dosdispensing/ApseRetryAdapter/trunk/ApseRetryAdapter/src/main/java/se/skltp/adapterservices/druglogistics/dosdispensing/ApseRetryComponent.java
+++ AdapterServices/riv.druglogistics.dosdispensing/ApseRetryAdapter/trunk/ApseRetryAdapter/src/main/java/se/skltp/adapterservices/druglogistics/dosdispensing/ApseRetryComponent.java
@@ -3,14 +3,9 @@ package se.skltp.adapterservices.druglogistics.dosdispensing;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.security.KeyManagementException;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
 import java.util.Date;
 
 import javax.net.ssl.SSLContext;
@@ -29,6 +24,7 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLContexts;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.slf4j.Logger;
@@ -40,20 +36,17 @@ public class ApseRetryComponent {
 
 	private static final Logger log = LoggerFactory.getLogger(ApseRetryComponent.class);
 
-	// private static String endpointAddress =
-	// "https://localhost:20001/loadtest/testproducer/GetSubjectOfCareSchedule/1/rivtabp21";
-	private static String endpointAddress = "https://localhost:8082/ApseRetryAdapter/services/hamtaMeddelande-soap-teststub/v1";
-	// private static String endpointAddress =
-	// "https://192.168.19.10:20000/vp/HamtaMeddelanden/1/rivtabp20";
-	private static String keystorePath = "src/consumer.jks";
-	private static String keystoreType = "JKS";
-	private static String keystorePassword = "password";
-	private static String truststorePath = "src/truststore.jks";
-	private static String truststoreType = "JKS";
-	private static String truststorePassword = "password";
-
 	@SuppressWarnings("unused")
 	private static int errors = 0;
+
+	/*
+	 * Configurable properties
+	 */
+	
+	private String endpointAddress = null;
+	public void setEndpointAddress(String endpointAddress) {
+		this.endpointAddress = endpointAddress;
+	}
 
 	private int maxRetries = -1;
 	public void setMaxRetries(int maxRetries) {
@@ -67,237 +60,153 @@ public class ApseRetryComponent {
 		this.timeout = timeout;
 	}
 	
-	
-	public String performPost(String request) throws Exception{
+	private String soapAction = null;
+	public void setSoapAction(String soapAction) {
+		this.soapAction = soapAction;
+	}
+
+	private String keystorePath = null;
+	public void setKeystorePath(String keystorePath) {
+		this.keystorePath = keystorePath;
+	}
+
+	private String keystoreType = null;
+	public void setKeystoreType(String keystoreType) {
+		this.keystoreType = keystoreType;
+	}
+
+	private String keystorePassword = null;
+	public void setKeystorePassword(String keystorePassword) {
+		this.keystorePassword = keystorePassword;
+	}
+
+	private String truststorePath = null;
+	public void setTruststorePath(String truststorePath) {
+		this.truststorePath = truststorePath;
+	}
+
+	private String truststoreType = null;
+	public void setTruststoreType(String truststoreType) {
+		this.truststoreType = truststoreType;
+	}
+
+	private String truststorePassword = null;
+	public void setTruststorePassword(String truststorePassword) {
+		this.truststorePassword = truststorePassword;
+	}
+
+
+	/**
+	 * Main method of the component. Make a http(s) post-request.
+	 * 
+	 * @param request
+	 */
+	public String performPost(String request) {
 		
 		try {
-			log.debug("### Request: [{}]", request);
-			String result = performPost(new StringEntity(request, "UTF-8"), 0);
-			log.debug("### Response: [{}]", result);
+			log.debug("Request: [{}]", request);
+
+			HttpClientContext context    = HttpClientContext.create();
+			HttpClient        httpClient = setupHttpClient();
+			HttpPost          httpPost   = setupHttpPost(setupHttpRequestEntity(request));
+			String            result     = performPostWithRetries(context, httpClient, httpPost);
+			
+			log.debug("Response: [{}]", result);
 			return result;
+			
 		} catch (RuntimeException e) {
-			log.debug("### Runtime Error: [{}]", e.toString());
+			if (e.getCause() == null) {
+				log.debug("Call failed on runtime error: [{}]", e.toString());
+			} else {
+				log.debug("Call failed on runtime cause error: [{}]", e.getCause().toString());
+			}
 			throw e;
-		} catch (UnsupportedEncodingException e) {
-			RuntimeException error = new RuntimeException("Error creating HttpEntity from request", e);
-			log.debug("### Error: [{}]", error.toString());
-			throw error;
-		}
-	}
-
-	private String performPost(HttpEntity requestEntity, int retries) throws Exception{
-		
-		if (endpointAddress.startsWith("https")) {
-			return performHttpsPost(requestEntity, retries);
-		} else {
-			return performHttpPost(requestEntity, retries);
 		}
 	}
 
 	/**
-	 * Make a http-request
+	 * Make a http(s) post-request with retry logic
 	 * 
-	 * @param requestEntity
-	 * @param retries
+	 * @param context
+	 * @param httpclient
+	 * @param httpPost
+	 * @return
 	 */
-	private String performHttpPost(HttpEntity requestEntity, int retries) throws Exception {
-		long ts = System.currentTimeMillis();
-		long id = Thread.currentThread().getId();
+	private String performPostWithRetries(HttpClientContext context, HttpClient httpclient, HttpPost httpPost)  {
 
+		boolean ok = false;
+		int retries = 0;
+		String response = null;
+		
+		// Repeat until ok or maxRetries is passed and an exception is thrown
+		do {
+			try {
+				System.err.println("Attempt: " + retries);
+				response = attemptOnePostRequest(context, httpclient, httpPost);
+				System.err.println("Attempt: " + retries + " OK!");
+				ok = true;
+
+			} catch (RuntimeException e) {
+				System.err.println("Attempt: " + retries + " failed!");
+
+				String errMsg = (e.getCause() == null) ? e.toString() : e.getCause().toString();
+					
+				if (retries == 0) {
+					errors++;
+					retries++;
+					log.warn("First request failed, retrying... Error Message: {}", errMsg);
+
+				} else if (retries < maxRetries) {
+					retries++;
+					log.warn("Retry " + retries + " failed, retrying... Error Message: {}", errMsg);
+
+				} else {
+					log.error("Request failed after {} retries. Giving up! Error Message: {}", retries, errMsg);
+					throw e;
+				}
+			} finally {
+				System.err.println("Attempt: " + retries + " pass finally");
+			}
+			
+		} while (!ok);
+		
+		return response;
+	}
+
+	/**
+	 * Try one http post request...
+	 * 
+	 * @param context
+	 * @param httpclient
+	 * @param httpPost
+	 * @return
+	 */
+	private String attemptOnePostRequest(HttpClientContext context, HttpClient httpclient, HttpPost httpPost) {
 		try {
-
-			HttpClientContext context = HttpClientContext.create();
-
-			HttpClient httpclient = HttpClients.custom().setRetryHandler(new DefaultHttpRequestRetryHandler(0, false))
-					.build();
-
-			HttpPost httpPost = new HttpPost(endpointAddress);
-
-			if (timeout > 0) {
-				log.debug("Setup connect and request timeout to {} ms.", timeout);
-				RequestConfig requestConfig = RequestConfig.custom()
-			    		.setConnectTimeout(timeout)
-			    		.setSocketTimeout(timeout)
-			            .build();
-				httpPost.setConfig(requestConfig);
-			}
-			
-			httpPost.setEntity(requestEntity);
-			httpPost.setHeader("Content-Type", CONTENT_TYPE);
-			httpPost.setHeader("SOAPAction",
-					"urn:riv:druglogistics:dosedispensing:HamtaMeddelandenResponder:1:HamtaMeddelanden");
-
 			HttpResponse response = httpclient.execute(httpPost, context);
-
-			HttpEntity responseEntity = response.getEntity();
-			int retCode = response.getStatusLine().getStatusCode();
-			String content = getContent(responseEntity);
-
-			// Check for errors
-			validate(retCode, content);
-
-			logOkRequest(ts, id, "Status = " + retCode);
-			
-			return content;
-			
+			String result = validateAndGetContent(response);
+			return result; 
 		} catch (Exception e) {
-
-			logErrorRequest(ts, id, e);
-			e.printStackTrace();
-
-			if (retries == 0) {
-				errors++;
-
-				log.warn("First request failed. Retrying...");
-				return performPost(requestEntity, ++retries);
-
-			} else if (retries < maxRetries) {
-				log.warn("Retry " + retries + " failed. Retrying...");
-				return performPost(requestEntity, ++retries);
-
-			} else {
-				log.error("Request failed. Giving up....");
-				throw e;
-			}
-
+			throw new RuntimeException(e);
 		}
 	}
 
 	/**
-	 * Make a https-request
+	 * Validate the response and get the content
 	 * 
-	 * @param requestEntity
-	 * @param retries
+	 * @param response
+	 * @return
 	 */
-	private String performHttpsPost(HttpEntity requestEntity, int retries) throws Exception {
-		
-		
-		long ts = System.currentTimeMillis();
-		long id = Thread.currentThread().getId();
+	private String validateAndGetContent(HttpResponse response) {
+		HttpEntity responseEntity = response.getEntity();
+		int retCode = response.getStatusLine().getStatusCode();
+		String content = getContent(responseEntity);
 
-		try {
+		// Check for errors
+		validate(retCode, content);
 
-			Registry<ConnectionSocketFactory> socketFactoryRegistry = setupSSLSocketFactory();
-
-			HttpClientConnectionManager connManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
-
-			HttpClientContext context = HttpClientContext.create();
-
-			HttpClient httpclient = HttpClients.custom().setRetryHandler(new DefaultHttpRequestRetryHandler(0, false))
-					.setConnectionManager(connManager).build();
-
-			HttpPost httpPost = new HttpPost(endpointAddress);
-
-			if (timeout > 0) {
-				log.debug("Setup connect and request timeout to {} ms.", timeout);
-				RequestConfig requestConfig = RequestConfig.custom()
-			    		.setConnectTimeout(timeout)
-			    		.setSocketTimeout(timeout)
-			            .build();
-				httpPost.setConfig(requestConfig);
-			}
-			
-			httpPost.setEntity(requestEntity);
-			httpPost.setHeader("Content-Type", CONTENT_TYPE);
-			httpPost.setHeader("SOAPAction",
-					"urn:riv:druglogistics:dosedispensing:HamtaMeddelandenResponder:1:HamtaMeddelanden");
-
-			HttpResponse response = httpclient.execute(httpPost, context);
-
-			HttpEntity responseEntity = response.getEntity();
-			int retCode = response.getStatusLine().getStatusCode();
-			String content = getContent(responseEntity);
-
-			// Check for errors
-			validate(retCode, content);
-
-			logOkRequest(ts, id, "Status = " + retCode);
-			
-			return content;
-			
-		} catch (Exception e) {
-
-			logErrorRequest(ts, id, e);
-			e.printStackTrace();
-
-			if (retries == 0) {
-				errors++;
-
-				log.warn("First request failed. Retrying...");
-				return performPost(requestEntity, ++retries);
-
-			} else if (retries < maxRetries) {
-				log.warn("Retry " + retries + " failed. Retrying...");
-				return performPost(requestEntity, ++retries);
-
-			} else {
-				log.error("Request failed. Giving up....");
-				throw e;
-			}
-		}
-	}
-
-	private Registry<ConnectionSocketFactory> setupSSLSocketFactory() throws Exception, NoSuchAlgorithmException,
-			KeyManagementException, KeyStoreException, UnrecoverableKeyException {
-		KeyStore keystore = load(keystorePath, keystorePassword.toCharArray(), keystoreType);
-		char[] keyPassword = keystorePassword.toCharArray();
-		KeyStore truststore = load(truststorePath, truststorePassword.toCharArray(), truststoreType);
-
-		SSLContext sslContext = SSLContexts.custom().loadKeyMaterial(keystore, keyPassword)
-				.loadTrustMaterial(truststore).build();
-
-		Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
-				.<ConnectionSocketFactory> create()
-				.register(
-						"https",
-						new SSLConnectionSocketFactory(sslContext,
-								SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)).build();
-
-		return socketFactoryRegistry;
-	}
-
-	private KeyStore load(final String res, final char[] passwd, String type) throws Exception {
-		final KeyStore keystore = KeyStore.getInstance(type);
-		keystore.load(new FileInputStream(res), passwd);
-		return keystore;
-	}
-
-	@SuppressWarnings("unused")
-	static private String streamToString(InputStream in) throws IOException {
-		StringBuilder out = new StringBuilder();
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		for (String line = br.readLine(); line != null; line = br.readLine())
-			out.append(line);
-		br.close();
-		return out.toString();
-	}
-
-	private static int okCnt = 0;
-
-	private void logOkRequest(long ts, long id, String msg) {
-		okCnt++;
-		log(ts, id, "OK: " + msg);
-	}
-
-	private static int errorCnt = 0;
-
-	private void logErrorRequest(long ts, long id, Exception e) {
-		errorCnt++;
-		log(ts, id, "ERROR: " + e.getMessage());
-	}
-
-	private void log(long ts, long id, String msg) {
-		if (Thread.currentThread().getId() != id) {
-			System.err.println("### ERROR THREAD ID INCORRECT");
-		}
-
-		int threadCnt = Thread.activeCount();
-		System.out.println("\n\n" + new Date() + ": (" + threadCnt + "/" + okCnt + "/" + errorCnt + ") " + getTs(ts)
-				+ ": " + msg);
-	}
-
-	private static long getTs(long ts) {
-		return System.currentTimeMillis() - ts;
+		logOkRequest("Status = " + retCode);
+		return content;
 	}
 
 	private String getContent(HttpEntity entity) {
@@ -327,13 +236,121 @@ public class ApseRetryComponent {
 	private void validate(int retCode, String content) {
 
 		if (retCode >= 400) {
+			System.err.println("### HTTP ERROR CODE DETECTED, THROW ERROR");
 			throw new RuntimeException("Bad status code: [" + retCode + "] with response [" + content + "]");
 		}
 
 		// Error checking for soapfault. Apoteket responds 200 OK , even for
 		// soapfaults!
 		if (content.contains("faultcode")) {
+			System.err.println("### SOAP FAULT DETECTED IN OK RESPONSE, THROW ERROR");
 			throw new RuntimeException("We got a SoapFault: [" + content + "] with status code [" + retCode + "]");
 		}
 	}
+
+	/*
+	 * Setup methods
+	 */
+
+	private StringEntity setupHttpRequestEntity(String request) {
+		try {
+			return new StringEntity(request, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private HttpPost setupHttpPost(HttpEntity requestEntity) {
+		HttpPost httpPost = new HttpPost(endpointAddress);
+
+		if (timeout > 0) {
+			log.debug("Setup connect and request timeout to {} ms.", timeout);
+			RequestConfig requestConfig = RequestConfig.custom()
+		    		.setConnectTimeout(timeout)
+		    		.setSocketTimeout(timeout)
+		            .build();
+			httpPost.setConfig(requestConfig);
+		}
+		
+		httpPost.setEntity(requestEntity);
+		httpPost.setHeader("Content-Type", CONTENT_TYPE);
+		httpPost.setHeader("SOAPAction", soapAction);
+		return httpPost;
+	}
+
+	private HttpClient setupHttpClient() {
+
+		HttpClientBuilder httpClientBuilder = HttpClients.custom().setRetryHandler(new DefaultHttpRequestRetryHandler(0, false));
+		if (endpointAddress.toLowerCase().startsWith("https")) {
+			httpClientBuilder = httpClientBuilder.setConnectionManager(setupSSLConnectionManager());
+		}
+		
+		HttpClient httpclient = httpClientBuilder.build();
+		return httpclient;
+	}
+
+	private HttpClientConnectionManager setupSSLConnectionManager() {
+		Registry<ConnectionSocketFactory> socketFactoryRegistry = setupSSLSocketFactory();
+
+		HttpClientConnectionManager connManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
+		return connManager;
+	}
+
+	private Registry<ConnectionSocketFactory> setupSSLSocketFactory() {
+		try {
+			KeyStore keystore   = loadKeystore(keystorePath, keystorePassword.toCharArray(), keystoreType);
+			char[] keyPassword  = keystorePassword.toCharArray();
+			KeyStore truststore = loadKeystore(truststorePath, truststorePassword.toCharArray(), truststoreType);
+
+			SSLContext sslContext = SSLContexts.custom().loadKeyMaterial(keystore, keyPassword).loadTrustMaterial(truststore).build();
+
+			Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
+				.<ConnectionSocketFactory> create()
+				.register("https", new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)).build();
+
+			return socketFactoryRegistry;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private KeyStore loadKeystore(final String res, final char[] passwd, String type) {
+		try {
+			final KeyStore keystore = KeyStore.getInstance(type);
+			keystore.load(new FileInputStream(res), passwd);
+			return keystore;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/*
+	 * Logger methods
+	 */
+	// TODO: Cleanup the methods below, we don't need most of it!
+	
+	private static int okCnt = 0;
+
+	private void logOkRequest(String msg) {
+		okCnt++;
+		log("OK: " + msg);
+	}
+
+	private static int errorCnt = 0;
+
+	private void logErrorRequest(Exception e) {
+		errorCnt++;
+		log("ERROR: " + e.getMessage());
+	}
+
+	private void log(String msg) {
+
+		int threadCnt = Thread.activeCount();
+		System.out.println("\n\n" + new Date() + ": (" + threadCnt + "/" + okCnt + "/" + errorCnt + "): " + msg);
+	}
+
+	private static long getTs(long ts) {
+		return System.currentTimeMillis() - ts;
+	}
+
 }
