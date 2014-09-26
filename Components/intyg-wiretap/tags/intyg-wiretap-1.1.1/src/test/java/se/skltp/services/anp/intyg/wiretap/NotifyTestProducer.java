08a3c8888e880fa406059b87bcc1257d59d60ecf
hans.thunberg <hans.thunberg@callistaenterprise.se>
2014-09-26 09:24:43 +0000

    [maven-scm] copy for tag intyg-wiretap-1.1.1

---
+++ Components/intyg-wiretap/tags/intyg-wiretap-1.1.1/src/test/java/se/skltp/services/anp/intyg/wiretap/NotifyTestProducer.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2014 Center for eHalsa i samverkan (CeHis).
+ * 							<http://cehis.se/>
+ *
+ * This file is part of SKLTP.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package se.skltp.services.anp.intyg.wiretap;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.ws.Provider;
+import javax.xml.ws.WebServiceProvider;
+
+import org.xml.sax.SAXException;
+
+@WebServiceProvider
+public class NotifyTestProducer implements Provider<DOMSource> {
+
+	@Override
+	public DOMSource invoke(DOMSource request) {
+		try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            DOMSource source = new DOMSource(builder.parse(new File(inputFile)));
+            
+            return source;
+        } catch (ParserConfigurationException e) {
+            throw wrap(e);
+        } catch (SAXException e) {
+            throw wrap(e);
+        } catch (IOException e) {
+            throw wrap(e);
+		}
+	}
+
+	private RuntimeException wrap(Exception e) {
+		return new RuntimeException();
+		
+	}
+
+	public static String inputFile;
+}
